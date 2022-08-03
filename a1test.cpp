9060db3e91dcfea6a02fbf140b00dee3e9e37fa4
ACnoway <imlzhyt@163.com>
2022-08-03 15:28:51 +0800

    ./a1test

--- a1test.cpp
+++ a1test.cpp
@@ -5,14 +5,6 @@ int f,n,k;
 int e[N],ne[N];
 int main()
 {
-    cin>>f>>n>>k;
-    for(int i=0;i<n;++i){
-        int addr;
-        cin>>addr;
-        cin>>e[addr]>>ne[addr];
-    }
-    for(int i=f;~i;i=ne[i]){
-        printf("%05d %d %05d\n",i,e[i],ne[i]);
-    }
+    cout<<"Hello World!"<<endl;
     return 0;
 }
