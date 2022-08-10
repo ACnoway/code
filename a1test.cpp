2a7c15e84cf242cf9955fe626ba1e91766c4a45c
ACnoway <imlzhyt@163.com>
2022-08-10 14:56:03 +0800

    Luogu/P1851 好朋友

--- a1test.cpp
+++ a1test.cpp
@@ -1,22 +1,20 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-const int N=100005;
-int f,n,k;
-int e[N],ne[N];
-void work(string x){
-    if(isdigit(x[0])){
-        int a=0;
-        for(int i=0;i<x.size();++i){
-            a=a*10+x[i]-'0';
-        }
-        cout<<(char)a<<endl;
-    }
-    else cout<<(int)x[0];
-}
+int s,a[100007];//这里取的10 0000，可以取更大
+//但即使对于打表来说，时间也够长的了
 int main()
 {
-    string s;
-    cin>>s;
-    work(s);
-    return 0;
+	for(int i=1;i<=100000;i++)
+	{
+		for(int j=1;j<=i;j++)
+		if(i%j==0)//判断j是i的余数
+		if(i!=j)//由题可得（仿佛是道数学题），约数没有本身
+		a[i]+=j;//其约数和加上j
+	}
+	for(int i=1;i<=100000;i++)
+	for(int j=1;j<=100000;j++)//双重循环判断
+	if(a[i]==j&&a[j]==i&&i>=6&&i>j&&j>=6)cout<<i<<' '<<j<<endl;
+    	//i的约数和等于j，j的约数和等于i，两者都大于6
+        //这里为了方便起见是i大于j
+	return 0;
 }
