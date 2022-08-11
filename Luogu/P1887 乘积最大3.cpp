7bfc23a6e80dc3486bfdba4a908421990ae9ee1d
ACnoway <imlzhyt@163.com>
2022-08-11 08:48:26 +0800

    Luogu/P1887 乘积最大3

---
+++ Luogu/P1887 乘积最大3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#else
+#define debug(x) cout<<' '<<#x<<'='<<x<<endl;
+#endif
+using namespace std;
+double n,m,aver;
+int main()
+{
+    cin>>n>>m;
+    aver=n/m;
+    while(n&&m){
+        cout<<floor(aver)<<' ';
+        n-=floor(aver);
+        m--;
+        aver=n/m;
+    }
+    return 0;
+}
