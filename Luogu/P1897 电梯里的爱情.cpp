bc2d2a030bfd230c44b12352d285dcf467cac8dc
ACnoway <imlzhyt@163.com>
2022-08-11 09:38:31 +0800

    Luogu/P1897 电梯里的爱情

--- Luogu/P1897 电梯里的爱情.cpp
+++ Luogu/P1897 电梯里的爱情.cpp
@@ -1,31 +1,38 @@
 #include<iostream>
 #include<cstdio>
-#include<algorithm>
-#include<cmath>
-#ifdef ONLINE_JUDGE
-#define debug(x)
-#else
-#define debug(x) cout<<' '<<#x<<'='<<x<<endl;
-#endif
 using namespace std;
 int n,ans,ma;
 bool v[20000007];
+inline int read(){
+    int x=0,f=1;
+    char c=getchar();
+    while(c<'0'||c>'9'){
+        if(c=='-') f=-1;
+        c=getchar();
+    }
+    while(c>='0'&&c<='9'){
+        x=(x<<3)+(x<<1)+(c^48);
+        c=getchar();
+    }
+    return x*f;
+}
+inline void write(int x){
+    if(x<0){x=-x;putchar('-');}
+    if(x>9) write(x/10);
+    putchar(x%10+'0');
+}
 int main()
 {
-    cin>>n;
+    n=read();
     int x;
     for(int i=0;i<n;++i){
-        cin>>x;
+        x=read();
         ma=max(ma,x);
-        if(v[x]){
-            ans++;
-        }
-        else{
+        if(!v[x]){
             v[x]=1;
-            ans+=6;
+            ans+=5;
         }
     }
-    ans+=ma*10;
-    cout<<ans<<endl;
+    write(ans+(ma<<3)+(ma<<1)+n);
     return 0;
 }
