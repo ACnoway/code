9c3ff1a5e6eb1e6eeb651270deea5c4f8d7e08cb
ACnoway <imlzhyt@163.com>
2022-08-15 16:43:37 +0800

    Luogu/P6367 [COCI2006-2007#6] PRASE

--- Luogu/P6367 [COCI2006-2007#6] PRASE.cpp
+++ Luogu/P6367 [COCI2006-2007#6] PRASE.cpp
@@ -2,15 +2,24 @@
 #include<cstdio>
 #include<algorithm>
 #include<cmath>
+#include<map>
 #ifdef ONLINE_JUDGE
 #define debug(x)
 #else
 #define debug(x) cout<<' '<<#x<<'='<<x<<endl;
 #endif
 using namespace std;
-
+int n,ans;
+map<string,int> m;
 int main()
 {
-    
+    cin>>n;
+    string s;
+    for(int i=0;i<n;++i){
+        cin>>s;
+        if(m[s]>i-m[s]) ans++;
+        m[s]++;
+    }
+    cout<<ans<<endl;
     return 0;
 }
