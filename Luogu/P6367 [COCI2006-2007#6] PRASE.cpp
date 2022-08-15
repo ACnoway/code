b5c12878f65a85bce442ac73d67d673aafffb32f
ACnoway <imlzhyt@163.com>
2022-08-15 16:40:57 +0800

    Luogu/P6367 [COCI2006-2007#6] PRASE

---
+++ Luogu/P6367 [COCI2006-2007#6] PRASE.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#else
+#define debug(x) cout<<' '<<#x<<'='<<x<<endl;
+#endif
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
