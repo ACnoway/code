e765b1308b18fe08ec3f9d89e53658c110abed23
ACnoway <imlzhyt@163.com>
2022-08-15 14:45:38 +0800

    ./a1test

--- a1test.cpp
+++ a1test.cpp
@@ -1,11 +1,70 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
 using namespace std;
-int s,a[100007];//这里取的10 0000，可以取更大
-//但即使对于打表来说，时间也够长的了
+inline int read(){
+    int x=0,f=1;
+    char c=getchar();
+    while(c<'0'||c>'9'){
+        if(c=='-') f=-1;
+        c=getchar();
+    }
+    while(c>='0'&&c<='9'){
+        x=(x<<3)+(x<<1)+(c^48);
+        c=getchar();
+    }
+    return x*f;
+}
+inline void write(int x){
+    if(x<0){x=-x;putchar('-');}
+    if(x>9) write(x/10);
+    putchar(x%10+'0');
+}
+int n,mn,mw,c1,c2,ans;
+int a1[100005][2],a2[100005][2];
 int main()
 {
-	string a="dabcdefg";
-    string b="dabc";
-    cout<<a.find_last_of(b)<<endl;
-	return 0;
+    n=read();
+    mn=read();
+    mw=read();
+    for(int i=1;i<=mn;++i){
+        int st=read(),en=read();
+        bool flag=false;
+        for(int i=1;i<=c1;++i){
+            if(st>a1[i][1]){
+                a1[i][1]=en;
+                a1[i][0]++;
+                flag=true;
+                break;
+            }
+        }
+        if(!flag){
+            a1[++c1][1]=en;
+            a1[c1][0]++;
+        }
+    }
+    for(int i=1;i<=mw;++i){
+        int st=read(),en=read();
+        bool flag=false;
+        for(int i=1;i<=c2;++i){
+            if(st>a2[i][1]){
+                a2[i][1]=en;
+                a2[i][0]++;
+                flag=true;
+                break;
+            }
+        }
+        if(!flag){
+            a2[++c2][1]=en;
+            a2[c2][0]++;
+        }
+    }
+    for(int i=1;i<=n;++i){
+        a1[i][0]+=a1[i-1][0];
+        a2[i][0]+=a2[i-1][0];
+    }
+    for(int i=1;i<=n;++i){
+        ans=max(a1[i][0]+a2[n-i][0],ans);
+    }
+    cout<<ans<<endl;
+    return 0;
 }
