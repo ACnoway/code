9791d7da40e87c73042f15377aa23bdeba0d77b9
ACnoway <imlzhyt@163.com>
2022-08-16 14:24:20 +0800

    Luogu/P6832 [Cnoi2020]子弦

---
+++ Luogu/P6745 『MdOI R3』Number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#else
+#define debug(x) cout<<' '<<#x<<'='<<x<<endl;
+#endif
+using namespace std;
+int k;
+string x;
+int main()
+{
+    cin>>k;
+    cin>>x;
+    
+    return 0;
+}
