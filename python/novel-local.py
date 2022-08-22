a73ccc1073f8534273cfffd37207e083c5c62b3c
ACnoway <imlzhyt@163.com>
2022-08-22 08:32:39 +0800

    python/novel-local

--- python/novel-local.py
+++ python/novel-local.py
@@ -1,3 +1,4 @@
+# 后端：https://quanxiaoshuo.com/
 import requests
 import re
 from bs4 import BeautifulSoup
@@ -19,7 +20,23 @@ class main_UI(QtWidgets.QWidget):
     def setup_UI(self):
         self.resize(500, 700)
         self.setWindowTitle("本地小说阅读器")
-        
+        self.layout = QtWidgets.QGridLayout()
+        #预览四个边都预留20pixs的边界
+        self.layout.setContentsMargins(20, 20, 20, 20)
+        #网格之间设置10pixs的间隔
+        self.layout.setSpacing(10)
+        self.layout.setRowMinimumHeight(0, 30)
+        self.search = QtWidgets.QLineEdit(self)
+        self.search.setPlaceholderText("请输入要搜索的书名，回车进行搜索")
+        self.search.setMinimumHeight(30)
+        self.layout.addWidget(self.search, 0, 0)
+        self.history = QtWidgets.QPushButton("阅读历史", self)
+        self.layout.addWidget(self.history, 1, 0)
+        self.exitt = QtWidgets.QPushButton("退出", self)
+        self.exitt.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        self.layout.addWidget(self.exitt, 2, 0)
+        self.setLayout(self.layout)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
