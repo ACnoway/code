bf94542cbb7328d311ba0acaa53a8412648e83be
ACnoway <imlzhyt@163.com>
2022-08-23 14:53:59 +0800

    ..\python\novel/main

---
+++ Luogu/T268841 S 组-单项选择.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+string ans;
+int main()
+{
+    //   000000000111111
+    //   123456789012345
+    ans="CCABDCCDDDCCBAB";
+    cout<<ans<<endl;
+    return 0;
+}
