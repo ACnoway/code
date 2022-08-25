1fc1fadebab1bbef28cfd27f12b07a71a4165346
ACnoway <imlzhyt@163.com>
2022-08-25 13:18:37 +0800

    Luogu/P1955 [NOI2015] 程序自动分析

---
+++ Luogu/P1955 [NOI2015] 程序自动分析.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<unordered_map>
+#define int long long
+using namespace std;
+const int N=400005;
+unordered_map<int,int> m;
+int p[N];
+int a[N][3];
+int b[N][2];
+int t,n;
+int find(int x){
+    if(p[x]!=x) p[x]=find(p[x]);
+    return p[x];
+}
+signed main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("237.txt", "r", stdin);
+    #endif
+    int ca,cb;
+    bool flag;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        ca=0;cb=0;
+        m.clear();
+        for(int i=0;i<N;++i) p[i]=i;
+        for(int i=1;i<=n;++i){
+            cin>>a[i][0]>>a[i][1]>>a[i][2];
+            if(!m[a[i][0]]) m[a[i][0]]=++ca;
+            if(!m[a[i][1]]) m[a[i][1]]=++ca;
+            if(a[i][2]==1){
+                p[find(m[a[i][0]])]=find(m[a[i][1]]);
+            }
+            else{
+                b[++cb][0]=a[i][0];
+                b[cb][1]=a[i][1];
+            }
+        }
+        flag=true;
+        for(int i=1;i<=cb;++i){
+            if(find(m[b[i][0]])==find(m[b[i][1]])){
+                flag=false;
+                break;
+            }
+        }
+        if(flag) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
