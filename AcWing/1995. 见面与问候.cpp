a364824dd0d31998778980093e7ba8fe80656986
AC？别闹！ <91535478+ACnoway@users.noreply.github.com>
2022-09-02 19:02:38 +0800

    AcWing/1995. 见面与问候

--- AcWing/1995. 见面与问候.cpp
+++ AcWing/1995. 见面与问候.cpp
@@ -3,26 +3,36 @@ using namespace std;
 int b[1000006],e[1000006];
 int main()
 {
-    int n,m,cb=0,ce=0;
+    int n,m,cb=0,ce=0,d;
+    char p;
     cin>>n>>m;
     for(int i=1;i<=n;++i){
-        int d;
-        char p;
         cin>>d>>p;
         for(int j=1;j<=d;++j){
-            if(p=='L') b[++cb]=b[cb-1]-1;
-            else b[++cb]=b[cb-1]+1;
+            ++cb;
+            if(p=='L') b[cb]=b[cb-1]-1;
+            else b[cb]=b[cb-1]+1;
         }
     }
-    for(int i=1;i<=n;++i){
-        int d;
-        char p;
+    for(int i=1;i<=m;++i){
         cin>>d>>p;
         for(int j=1;j<=d;++j){
-            if(p=='L') e[++ce]=e[ce-1]-1;
-            else e[++ce]=e[ce-1]+1;
+            ++ce;
+            if(p=='L') e[ce]=e[ce-1]-1;
+            else e[ce]=e[ce-1]+1;
         }
     }
-    
+    if(ce>cb){
+        for(int i=cb+1;i<=ce;++i) b[i]=b[i-1];
+    }
+    if(ce<cb){
+        for(int i=ce+1;i<=cb;++i) e[i]=e[i-1];
+    }
+    cb=max(cb,ce);
+    int ans=0;
+    for(int i=1;i<=cb;++i){
+        if(b[i]==e[i]&&b[i-1]!=e[i-1]) ans++;
+    }
+    cout<<ans<<endl;
     return 0;
 }
