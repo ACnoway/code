788697313a94238df5d812ff3c58c3c0bd37c84a
AC？别闹！ <91535478+ACnoway@users.noreply.github.com>
2022-09-22 18:38:12 +0800

    AcWing/899. 编辑距离

--- AcWing/899. 编辑距离.cpp
+++ AcWing/899. 编辑距离.cpp
@@ -1,33 +1,28 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 using namespace std;
-int n,m;
+int n,m,ans=0,op,nn,mm;
 int f[20][20];
-char a[1003][20];
+char a[1003][20],b[20];
 int main()
 {
     cin>>n>>m;
     for(int i=0;i<n;++i) cin>>a[i]+1;
-    int ans=0,op;
     while(m--){
-        char b[20];
-        cin>>b+1>>op;
         ans=0;
+        cin>>b+1>>op;
+        mm=strlen(b+1);
         for(int t=0;t<n;++t){
-            for(int i=1;i<=strlen(a[t]);++i) f[i][0]=i;
-            for(int i=1;i<=strlen(b);++i) f[0][i]=i;
-            for(int i=1;i<=strlen(a[t]);++i){
-                for(int j=1;j<=strlen(b);++j) f[i][j]=0;
-            }
-            for(int i=1;i<=strlen(a[t]);++i){
-                for(int j=1;j<=strlen(b);++j){
+            nn=strlen(a[t]+1);
+            for(int i=1;i<=nn;++i) f[i][0]=i;
+            for(int i=1;i<=mm;++i) f[0][i]=i;
+            for(int i=1;i<=nn;++i){
+                for(int j=1;j<=mm;++j){
                     f[i][j]=min(f[i-1][j],f[i][j-1])+1;
-                    if(a[t][i-1]==b[j-1]) f[i][j]=min(f[i][j],f[i-1][j-1]);
-                    else f[i][j]=min(f[i][j],f[i-1][j-1]+1);
+                    f[i][j]=min(f[i][j],f[i-1][j-1]+(a[t][i]!=b[j]));
                 }
             }
-            cout<<f[strlen(a[t])][strlen(b)]<<endl;
-            if(f[strlen(a[t])][strlen(b)]<=op) ans++;
+            if(f[nn][mm]<=op) ans++;
         }
         cout<<ans<<endl;
     }
