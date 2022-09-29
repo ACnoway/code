4b18d7c5ac539b0ad118de0ca2b67afd926e64d8
AC？别闹！ <91535478+ACnoway@users.noreply.github.com>
2022-09-29 18:05:55 +0800

    template/最长公共子序列LCS

---
+++ template/最长公共子序列LCS.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int n,m;
+char a[1003],b[1003];
+int f[1003][1003];
+int main()
+{
+    cin>>a+1>>b+1;
+    n=strlen(a+1);
+    m=strlen(b+1);
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=m;++j){
+            f[i][j]=max(f[i-1][j],f[i][j-1])+1;
+            if(a[i]==b[j]) f[i][j]=max(f[i][j],)
+        }
+    }
+    return 0;
+}
