affdbf55bdd7bad8641ecb3e15ee5ff93c900133
ACnoway <imlzhyt@163.com>
2022-10-06 11:09:19 +0800

    ./test

--- test.bat
+++ test.bat
@@ -3,7 +3,8 @@
 ::%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 ::设置要永久加入到path环境变量中的路径
 ::要以管理员权限运行
-set My_PATH=E:\lou\software\Git\cmd\;E:\lou\software\Git\;E:\lou\software\TDM-GCC10.3.0\bin\;
+set MY_PATH=E:\lou\software\Git\cmd\;E:\lou\software\Git\;E:\lou\software\TDM-GCC10.3.0\bin\;
  
-set PATH=%My_PATH%%PATH%;
+set PATH=%MY_PATH%%PATH%;
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v "Path" /t REG_EXPAND_SZ /d "%PATH%" /f
+pause
