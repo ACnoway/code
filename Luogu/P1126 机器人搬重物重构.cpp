61650ad0fc07ee5a2461956a71bc6302f88c1609
ACnoway <imlzhyt@163.com>
2022-10-15 16:57:44 +0800

    Luogu/P1126 机器人搬重物重构

--- Luogu/P1126 机器人搬重物重构.cpp
+++ Luogu/P1126 机器人搬重物重构.cpp
@@ -18,6 +18,15 @@ bool v[55][55][5];
 struct node{
     int x,y,num,fa;
 };
+inline bool check(int x,int y){
+    for(int i=0;i<4;++i){
+        if(x+ck[i][0]<1||x+ck[i][0]>n||y+ck[i][1]<1||y+ck[i][1]>m) return 0;
+        if(a[x+ck[i][0]][y+ck[i][1]]){
+            return 0;
+        }
+    }
+    return 1;
+}
 int main()
 {
     cin>>n>>m;
@@ -26,6 +35,19 @@ int main()
     }
     cin>>xa>>ya>>xb>>yb>>facest;
     for(int i=0;i<4;++i) if(way[i]==facest) face=i;
-    
+    queue<node> q;
+    node p;
+    int nx,ny;
+    q.push({xa,ya,0,face});
+    v[xa][ya][face]=1;
+    int ans=2147483647;
+    if(!check(xa,ya)){
+        cout<<-1<<endl;
+        return 0;
+    }
+    while(!q.empty()){
+        p=q.front();
+        
+    }
     return 0;
 }
