c19186725e944249eab2ae509d7451d3baa73423
ACnoway <imlzhyt@163.com>
2022-11-09 13:32:20 +0800

    正睿/22冲刺day3数字和

---
+++ Luogu/P8817 [CSP-S 2022] 假期计划重构.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#else
+#define debug(x) cout<<' '<<#x<<'='<<x<<endl;
+#endif
+using namespace std;
+const int N=2502;
+int n,m,k,x,y,ans,tmp;
+int f[2502][2502],ff[2502][2502];
+int a[2502];
+void bfs(){
+    
+}
+int main()
+{
+    
+    return 0;
+}
