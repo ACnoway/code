aa5b406312b0ab90aed909a7c29cf15c30374829
ACnoway <imlzhyt@163.com>
2022-11-09 19:20:29 +0800

    正睿\20测day3/B走迷宫

---
+++ 正睿/20测day3/B走迷宫.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cstdlib>
+#include<queue>
+using namespace std;
+int n,m,ans;
+int a[1003][1003];
+int dx[4]={1,0,-1,0},dy[4]={0,1,0,-1};
+bool v[1003][1003];
+typedef pair<int,int> pii;
+void bfs(int x,int y){
+	queue<pii> q;
+    pii p;
+    int nx,ny;
+    
+}
+int main()
+{
+	cin>>n>>m;
+	for(int i=0;i<=n;++i){
+		a[i][0]=a[0][i]=2000006;
+		a[i][n+1]=a[n+1][i]=2000006;
+	}	
+	for(int i=1;i<=n;++i){
+		for(int j=1;j<=m;++j) cin>>a[i][j];
+	}
+	bool flag=1;
+	for(int i=1;i<=n;++i){
+		for(int j=1;j<=m;++j){
+			if(v[i][j]) continue;
+			flag=1;
+			for(int k=0;k<4;++k){
+				if(a[i][j]-a[i+dx[k]][j+dy[k]]==2){
+					flag=0;
+					v[i][j]=1;
+					break;
+				}
+			}
+			if(!flag) continue;
+			bfs(i,j);
+		}
+	}
+	return 0;
+}
