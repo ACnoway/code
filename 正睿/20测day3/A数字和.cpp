bb5aa2b5c84c12770b594fe7389cd71516c48dca
ACnoway <imlzhyt@163.com>
2022-11-09 19:20:29 +0800

    正睿\20测day3/B走迷宫

---
+++ 正睿/20测day3/A数字和.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+const int N=200005;
+int n;
+long long a[N],qz[N],hz[N];
+int main()
+{
+	freopen("data.txt","r",stdin);
+	freopen("ans1.txt","w",stdout);
+	cin>>n;
+	for(int i=1;i<=n;++i){
+		cin>>a[i];
+		qz[i]=max(0ll,qz[i-1])+a[i];
+	}
+	for(int i=n;i>0;--i){
+		hz[i]=max(0ll,hz[i+1])+a[i];
+	}
+	for(int i=1;i<=n;++i) cout<<qz[i]+hz[i]-a[i]<<' ';
+	return 0;
+}
