b36fb1cebcffbde0bd172668b4e68a47f805c811
ACnoway <imlzhyt@163.com>
2023-01-10 15:06:26 +0800

    Luogu/P8817 [CSP-S 2022] 假期计划重构

--- Luogu/P8817 [CSP-S 2022] 假期计划重构.cpp
+++ Luogu/P8817 [CSP-S 2022] 假期计划重构.cpp
@@ -2,21 +2,93 @@
 #include<cstdio>
 #include<algorithm>
 #include<cmath>
+#include<vector>
+#include<cstring>
+#include<queue>
+#define int long long
 #ifdef ONLINE_JUDGE
 #define debug(x)
 #else
 #define debug(x) cout<<' '<<#x<<'='<<x<<endl;
 #endif
 using namespace std;
-const int N=2502;
-int n,m,k,x,y,ans,tmp;
-int f[2502][2502],ff[2502][2502];
-int a[2502];
-void bfs(){
-    
+inline int read(){
+    int x=0,f=1;
+    char c=getchar();
+    while(c<'0'||c>'9'){
+        if(c=='-') f=-1;
+        c=getchar();
+    }
+    while(c>='0'&&c<='9'){
+        x=(x<<3)+(x<<1)+(c^48);
+        c=getchar();
+    }
+    return x*f;
 }
-int main()
+inline void write(int x){
+    if(x<0){x=-x;putchar('-');}
+    if(x>9) write(x/10);
+    putchar(x%10+'0');
+}
+const int N=2502;
+int n,m,k,u,v;
+int a[N],dis[N];
+vector<int> f[N],g[N];
+bool can[N][N];
+bool cmp(int x,int y){
+    return a[x]>a[y];
+}
+void bfs(int x){
+    memset(dis,-1,sizeof(dis));
+    queue<int> q;
+    q.push(x);
+    dis[x]=0;
+    while(!q.empty()){
+        u=q.front();
+        q.pop();
+        if(u!=x){
+            can[x][u]=1;
+            if(x!=1&&can[1][u]){
+                f[x].push_back(u);
+                sort(f[x].begin(),f[x].end(),cmp);
+                if(f[x].size()>3) f[x].pop_back();
+            }
+        }
+        if(dis[u]>=k+1) continue;
+        for(int to:g[u]){
+            if(dis[to]==-1){
+                dis[to]=dis[u]+1;
+                q.push(to);
+            }
+        }
+    }
+}
+signed main()
 {
-    
+    n=read();m=read();k=read();
+    for(int i=2;i<=n;++i) a[i]=read();
+    for(int i=1;i<=m;++i){
+        u=read();v=read();
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    for(int i=1;i<=n;++i){
+        bfs(i);
+    }
+    int ans=0;
+    for(int b=2;b<=n;++b){
+        for(int c=2;c<=n;++c){
+            if(can[b][c]){
+                for(int aa:f[b]){
+                    for(int dd:f[c]){
+                        if(aa!=c&&dd!=b&&aa!=dd){
+                            ans=max(ans,a[aa]+a[b]+a[c]+a[dd]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout<<ans<<endl;
     return 0;
 }
--- a1test.cpp
+++ a1test.cpp
@@ -1,24 +1,117 @@
-#include<bits/stdc++.h>
-#ifdef ONLINE_JUDGE
-#define debug(x)
-#else
-#define debug(x) cout<<' '<<#x<<'='<<x<<endl;
-#endif
-using namespace std;
-int main()
+/*
+ * @Author: crab-in-the-northeast
+ * @Date: 2022-10-30 17:40:03
+ * @Last Modified by: crab-in-the-northeast
+ * @Last Modified time: 2022-10-30 19:07:52
+ */
+
+#include <bits/stdc++.h>
+#define int long long
+inline int read()
 {
-    freopen("p8818.txt","w",stdout);
-    srand(time(0));
-    int n=rand()%100+1,m=rand()%100+1,q=rand()%100+1;
-    cout<<n<<' '<<m<<' '<<q<<endl;
-    for(int i=1;i<=n;++i) cout<<(rand()%2?rand()%100+1:-1*(rand()%100+1))<<' ';
-    cout<<endl;
-    for(int i=1;i<=m;++i) cout<<(rand()%2?rand()%100+1:-1*(rand()%100+1))<<' ';
-    cout<<endl;
-    for(int i=0;i<q;++i){
-        int l1=rand()%n+1,l2=rand()%m+1;
-        int r1=rand()%(n-l1)+l1,r2=rand()%(m-l2)+l2;
-        cout<<l1<<' '<<r1<<' '<<l2<<' '<<r2<<endl;
+    int x = 0;
+    bool flag = true;
+    char ch = getchar();
+    while (!isdigit(ch))
+    {
+        if (ch == '-')
+            flag = false;
+        ch = getchar();
     }
+    while (isdigit(ch))
+    {
+        x = (x << 1) + (x << 3) + ch - '0';
+        ch = getchar();
+    }
+    if (flag)
+        return x;
+    return ~(x - 1);
+}
+
+const int maxn = 2505;
+typedef std ::pair<int, int> pii;
+
+std ::vector<int> G[maxn];
+int w[maxn];
+bool ok[maxn][maxn];       // u, v 是否可达
+std ::vector<int> f[maxn]; // f[u] 存放：可达 u 且可达 1 的前三大 v
+
+int k;
+
+int dis[maxn];
+
+void bfs(int x)
+{
+    std ::memset(dis, -1, sizeof(dis));
+    std ::queue<int> q;
+    q.push(x);
+    dis[x] = 0;
+
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+
+        if (u != x)
+        {
+            ok[x][u] = true;
+            if (x != 1 && ok[1][u])
+            {
+                // printf("%lld %lld\n", x, u);
+                f[x].push_back(u);
+                std ::sort(f[x].begin(), f[x].end(), [](int u, int v)
+                           { return w[u] > w[v]; }); // 注意这里 sort 元素数量不超过 3，效率可看做常数
+                if (f[x].size() > 3)
+                    f[x].pop_back();
+            }
+        }
+
+        if (dis[u] == k + 1)
+            continue;
+
+        for (int v : G[u])
+            if (dis[v] == -1)
+            {
+                dis[v] = dis[u] + 1;
+                q.push(v);
+            }
+    }
+}
+
+inline bool gmx(int &a, int b)
+{
+    return b > a ? a = b, true : false;
+}
+
+signed main()
+{
+    int n = read(), m = read();
+    k = read();
+    for (int u = 2; u <= n; ++u)
+        w[u] = read();
+
+    while (m--)
+    {
+        int u = read(), v = read();
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+
+    for (int u = 1; u <= n; ++u)
+        bfs(u);
+
+    int ans = 0;
+
+    for (int b = 2; b <= n; ++b)
+        for (int c = 2; c <= n; ++c)
+            if (ok[b][c])
+                for (int a : f[b])
+                    for (int d : f[c])
+                        if (a != c && b != d && a != d)
+                            // 其他不等关系天然满足了，只有这三组需要检验；
+                            // 天然满足的不等关系：a != b，b != c，c != d，请读者自己思考为什么。
+                            gmx(ans, w[a] + w[b] + w[c] + w[d]);
+
+    printf("%lld\n", ans);
     return 0;
 }
