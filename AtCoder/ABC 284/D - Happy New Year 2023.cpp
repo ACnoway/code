3331c5edd103dd793bcb8c3e3f42d56a61fb73fe
ACnoway <imlzhyt@163.com>
2023-01-11 11:47:34 +0800

    AtCoder\ABC 284/D - Happy New Year 2023

--- AtCoder/ABC 284/D - Happy New Year 2023.cpp
+++ AtCoder/ABC 284/D - Happy New Year 2023.cpp
@@ -11,32 +11,51 @@
 using namespace std;
 typedef unsigned long long ull;
 int t;
-ull n;
-ull primes[10000007];
-bool p[10000007];
-map<ull,bool> m;
+ull n,ss;
+ull primes[4000006];
+bool p[4000006];
+bool check(ull x){
+    if(x==1||(x%2==0&&x!=2)) return 0;
+    if(x==2) return 1;
+    for(ull i=3;i*i<=x;i+=2){
+        if(x%i==0) return 0;
+    }
+    return 1;
+}
 int main()
 {
-    for(int i=0;i<=10000000;++i) p[i]=1;
+    for(int i=0;i<=3000006;++i) p[i]=1;
     p[0]=p[1]=0;
-    for(int i=0;i<=10000000;++i){
-        if(p[i]){
-            primes[++primes[0]]=i;
-            m[i*i]=1;
-            for(int j=2*i;j<=10000000;j+=i){
-                p[j]=0;
-            }
+    for(int i=0;i<=3000006;++i){
+        if(p[i]) primes[++primes[0]]=i;
+        for(int j=1;j<=primes[0];++j){
+            if(i*primes[j]>3000006) break;
+            p[i*primes[j]]=0;
+            if(i%primes[j]==0) break;
         }
     }
+    //for(int i=1;i<=10;++i) cout<<primes[i]*primes[i]<<endl;
+    ///*
     cin>>t;
     while(t--){
         cin>>n;
-        for(int i=1;primes[i]<=n;++i){
-            if(n%primes[i]==0&&m[n/primes[i]]){
-                cout<<sqrt(n/primes[i])<<' '<<primes[i]<<endl;
-                break;
+        for(int i=1;i<=primes[0];++i){
+            if(n%primes[i]==0){
+                if(n%(primes[i]*primes[i])==0){
+                    ss=n/primes[i]/primes[i];
+                    cout<<primes[i]<<' '<<ss<<endl;
+                    break;
+                }
+                else{
+                    ss=sqrt(n/primes[i]);
+                    if(ss*ss==n/primes[i]&&check(ss)){
+                        cout<<ss<<' '<<primes[i]<<endl;
+                        break;
+                    }
+                }
             }
         }
     }
+    //*/
     return 0;
 }
