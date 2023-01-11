493f4ba0236b21b4fc7778bb29c137b0c57a2baa
ACnoway <imlzhyt@163.com>
2023-01-11 15:47:10 +0800

    ./.gitignore

---
+++ AcWing/4656. 技能升级.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#else
+#define debug(x) cout<<' '<<#x<<'='<<x<<endl;
+#endif
+using namespace std;
+int n,m;
+struct skill{
+    double a,b,c;
+}
+int main()
+{
+    
+    return 0;
+}
