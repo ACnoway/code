1202f53985f08e7014e8e890680876a0f7acba22
ACnoway <imlzhyt@163.com>
2023-01-12 08:08:48 +0800

    AcWing/4652. 纸张尺寸

--- AcWing/4652. 纸张尺寸.cpp
+++ AcWing/4652. 纸张尺寸.cpp
@@ -2,22 +2,20 @@
 #include<cstdio>
 #include<algorithm>
 #include<cmath>
-#define int long long
 #ifdef ONLINE_JUDGE
 #define debug(x)
 #else
 #define debug(x) cout<<' '<<#x<<'='<<x<<endl;
 #endif
 using namespace std;
-int n,m;
-struct skill{
-    double a,b,c;
-}t[100005];
+int a,b,c;
 int main()
 {
-    cin>>n>>m;
-    for(int i=1;i<=n;++i){
-        cin>>
+    scanf("A%d",&c);
+    a=1189;b=841;
+    for(int i=1;i<=c;++i){
+        a>>=1;if(a<b) swap(a,b);
     }
+    cout<<a<<endl<<b<<endl;
     return 0;
 }
