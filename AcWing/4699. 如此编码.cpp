55b083e86a240061bffb5ad54c7ae4e008a137bd
ACnoway <imlzhyt@163.com>
2023-01-12 11:02:48 +0800

    AcWing/4699. 如此编码

---
+++ AcWing/4699. 如此编码.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#else
+#define debug(x) cout<<' '<<#x<<'='<<x<<endl;
+#endif
+using namespace std;
+int n,m,bef;
+int a[30];
+int main()
+{
+    cin>>n>>m;
+    a[0]=1;
+    for(int i=1;i<=n;++i){
+        cin>>a[i];
+        a[i]*=a[i-1];
+        cout<<(m%a[i]-bef)/a[i-1]<<' ';
+        bef=m%a[i];
+    }
+    
+    return 0;
+}
