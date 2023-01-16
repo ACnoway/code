c293ddc98ff85ddd92a5fa7f8a2fdd5beb255e7f
ACnoway <imlzhyt@163.com>
2023-01-16 09:04:12 +0800

    Luogu/P8942 Digital Fortress

--- a1test.cpp
+++ a1test.cpp
@@ -1,13 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    freopen("P1156.in","w",stdout);
-    srand(time(0));
-    int d=rand()%99+2,g=rand()%100+1;
-    cout<<d<<' '<<g<<endl;
-    for(int i=1;i<=g;++i){
-        cout<<rand()%1000+1<<' '<<rand()%30+1<<' '<<rand()%25<<endl;
-    }
+    
     return 0;
 }
