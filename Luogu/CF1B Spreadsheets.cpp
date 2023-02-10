efaebfcee06ad46464e8f3b87059e35cd06d00e1
ACnoway <imlzhyt@163.com>
2023-02-10 15:53:17 +0800

    Luogu/CF1B Spreadsheets

--- Luogu/CF1B Spreadsheets.cpp
+++ Luogu/CF1B Spreadsheets.cpp
@@ -1,48 +1,43 @@
 #include<iostream>
+#include<cstdio>
+#include<algorithm>
 #include<cmath>
+#include<cstring>
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#else
+#define debug(x) cout<<' '<<#x<<'='<<x<<endl;
+#endif
 using namespace std;
-int n,tmp,flag,x[2];
-int ans[1000006];
-char zimu[27];
-string a,b,c;
+int t,x,y,xx,yy;
+char s[10000];
+string ans;
 int main()
 {
-    for(int i=1;i<=26;++i) zimu[i]=i+'A'-1;
-    zimu[0]='\0';
-    cin>>n;
-    while(n--){
-        cin>>a;
-        b="";
-        flag=0;
-        tmp=0;
-        x[0]=0;x[1]=0;
-        for(int i=0;i<a.size();++i){
-            if(a[i]=='C'&&i>0&&a[i-1]>='0'&&a[i-1]<='9') flag=1;
-        }
-        if(flag){
-            flag=0;
-            for(int i=0;i<a.size();++i){
-                if(a[i]>='0'&&a[i]<='9') x[flag]=(x[flag]<<3)+(x[flag]<<1)+(a[i]^48);
-                if(a[i]=='C') flag=1;
+    cin>>t;
+    while(t--){
+        cin>>s;
+        if(sscanf(s,"R%dC%d",&x,&y)==2){
+            ans="";
+            // y--;
+            while(y){
+                ans=(char)((y-1)%26+'A')+ans;
+                y=y/26;
             }
-            //x[1]--;
-            while(x[1]){
-                if(x[1]>26) b+='Z';
-                else b+=(x[1]-1)%26+'A';
-                //cout<<x[1]<<' '<<b<<endl;
-                x[1]/=27;
-            }
-            cout<<b<<x[0]<<endl;
+            cout<<ans<<x<<endl;
         }
         else{
-            for(int i=0;i<a.size();++i){
-                if(a[i]>='A'&&a[i]<='Z') b=a[i]+b;
-                else x[1]=(x[1]<<3)+(x[1]<<1)+(a[i]^48);
+            x=0;
+            int i;
+            for(i=0;i<strlen(s);++i){
+                if(s[i]>='0'&&s[i]<='9') break;
+                x=x*26+(s[i]-'A'+1);
             }
-            for(int i=0;i<b.size();++i){
-                x[0]+=(b[i]-'A'+1)*pow(26,i);
+            putchar('R');
+            for(;i<strlen(s);++i){
+                putchar(s[i]);
             }
-            cout<<'R'<<x[1]<<'C'<<x[0]<<endl;
+            cout<<'C'<<x<<endl;
         }
     }
     return 0;
