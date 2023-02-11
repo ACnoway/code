b4c71aeff3f2ed71f2f3c861a0c337ac26d1b8dd
ACnoway <imlzhyt@163.com>
2023-02-11 18:39:34 +0800

    Luogu/P9063 [yLOI2023] 分解只因数

---
+++ Luogu/P9066 [yLOI2023] 腐草为萤.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#else
+#define debug(x) cout<<' '<<#x<<'='<<x<<endl;
+#endif
+using namespace std;
+inline int read(){
+    int x=0,f=1;
+    char c=getchar();
+    while(c<'0'||c>'9'){
+        if(c=='-') f=-1;
+        c=getchar();
+    }
+    while(c>='0'&&c<='9'){
+        x=(x<<3)+(x<<1)+(c^48);
+        c=getchar();
+    }
+    return x*f;
+}
+inline void write(int x){
+    if(x<0){x=-x;putchar('-');}
+    if(x>9) write(x/10);
+    putchar(x%10+'0');
+}
+int n;
+int p[500005],a[500005];
+int main()
+{
+    n=read();
+    for(int i=1;i<=n;++i) p[i]=read();
+    for(int i=1;i<=n;++i) a[i]=read();
+    write((a[0]>a[1]?p[0]:p[1]));
+    return 0;
+}
