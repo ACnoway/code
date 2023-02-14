62236783a6f5ef5ac47b99dac0e3482b231bf894
ACnoway <imlzhyt@163.com>
2023-02-14 20:42:38 +0800

    Luogu/P2216 [HAOI2007]理想的正方形

---
+++ Luogu/P2216 [HAOI2007]理想的正方形.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#define int long long
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#else
+#define debug(x) cout<<' '<<#x<<'='<<x<<endl;
+#endif
+using namespace std;
+inline int read(){
+    int x=0,f=1;
+    char c=getchar();
+    while(c<'0'||c>'9'){
+        if(c=='-') f=-1;
+        c=getchar();
+    }
+    while(c>='0'&&c<='9'){
+        x=(x<<3)+(x<<1)+(c^48);
+        c=getchar();
+    }
+    return x*f;
+}
+inline void write(int x){
+    if(x<0){x=-x;putchar('-');}
+    if(x>9) write(x/10);
+    putchar(x%10+'0');
+}
+const int maxn=1003;
+int n,m,k;
+int a[maxn][maxn];
+int main()
+{
+    n=read();m=read();k=read();
+    for(int i=1;i<=n;++i) for(int j=1;j<=m;++j) a[i][j]=read();
+    
+    return 0;
+}
