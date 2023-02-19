e60ff8dd7d277e5e40d0ec6810bd80e9edc61303
ACnoway <imlzhyt@163.com>
2023-02-19 12:31:51 +0800

    LOJ/#6279. 数列分块入门 3

--- LOJ/#6279. 数列分块入门 3.cpp
+++ LOJ/#6279. 数列分块入门 3.cpp
@@ -27,37 +27,38 @@ inline void write(int x){
     if(x>9) write(x/10);
     putchar(x%10+'0');
 }
-const int maxn=50004;
+const int maxn=100005;
 int n,siz,sum;
 int a[maxn],bl[maxn],g[maxn][2],lt[maxn];
 vector<int> v[502];
 inline void add(int l,int r,int c){
-    for(int i=l;i<=min(bl[l]*siz,r);++i) a[i]+=c;
+    for(int i=l;i<=min(g[bl[l]][1],r);++i) a[i]+=c;
     v[bl[l]].clear();
-    for(int i=(bl[l]-1)*siz+1;i<=min(bl[l]*siz,n);++i) v[bl[l]].push_back(a[i]);
+    for(int i=g[bl[l]][0];i<=g[bl[l]][1];++i) v[bl[l]].push_back(a[i]);
     sort(v[bl[l]].begin(),v[bl[l]].end());
     if(bl[l]!=bl[r]){
-        for(int i=(bl[r]-1)*siz+1;i<=min(r,n);++i) a[i]+=c;
+        for(int i=g[bl[r]][0];i<=min(r,n);++i) a[i]+=c;
         v[bl[r]].clear();
-        for(int i=(bl[r]-1)*siz+1;i<=min(bl[r]*siz,n);++i) v[bl[r]].push_back(a[i]);
+        for(int i=g[bl[r]][0];i<=g[bl[r]][1];++i) v[bl[r]].push_back(a[i]);
         sort(v[bl[r]].begin(),v[bl[r]].end());
     }
     for(int i=bl[l]+1;i<=bl[r]-1;++i) lt[i]+=c;
 }
 inline void query(int l,int r,int c){
-    int cn=c,ans=-1;
+    int cn=c,ans=-2147483647;
     cn-=lt[bl[l]];
-    for(int i=l;i<=min(bl[l]*siz,r);++i) if(cn>a[i]) ans=max(ans,a[i]+lt[bl[l]]);
+    for(int i=l;i<=min(g[bl[l]][1],r);++i) if(cn>a[i]) ans=max(ans,a[i]+lt[bl[l]]);
     if(bl[l]!=bl[r]){
         cn=c-lt[bl[r]];
-        for(int i=(bl[r]-1)*siz+1;i<=r;++i) if(cn>a[i]) ans=max(ans,a[i]+lt[bl[r]]);
+        for(int i=g[bl[r]][0];i<=r;++i) if(cn>a[i]) ans=max(ans,a[i]+lt[bl[r]]);
     }
     for(int i=bl[l]+1;i<=bl[r]-1;++i){
         cn=c-lt[i];
         auto x=lower_bound(v[i].begin(),v[i].end(),cn);
         if(x!=v[i].begin()) ans=max(ans,*prev(x)+lt[i]);
     }
-    write(ans);
+    if(ans!=-2147483647) write(ans);
+    else write(-1);
     putchar('\n');
 }
 int main()
