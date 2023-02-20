1910bc675658ca595d52a215d0ef9841fdc90693
ACnoway <imlzhyt@163.com>
2023-02-20 15:53:39 +0800

    LOJ/#6283. 数列分块入门 7

--- LOJ/#6283. 数列分块入门 7.cpp
+++ LOJ/#6283. 数列分块入门 7.cpp
@@ -26,24 +26,44 @@ inline void write(int x){
     if(x>9) write(x/10);
     putchar(x%10+'0');
 }
-const int maxn=50004,mod=10007;
+const int maxn=100005,mod=10007;
 int n,siz,sum;
 int a[maxn],bl[maxn],jia[maxn],cheng[maxn],g[maxn][2];
 inline void add(int l,int r,int c){
-    for(int i=l;i<=min(g[bl[l]][1],r);++i) a[i]+=c,a[i]%=mod;
+    for(int i=g[bl[l]][0];i<=g[bl[l]][1];++i){
+        a[i]=a[i]*cheng[bl[l]]+jia[bl[l]]+(l<=i&&i<=r?c:0);
+        a[i]%=mod;
+    }
+    cheng[bl[l]]=1;
+    jia[bl[l]]=0;
     if(bl[l]!=bl[r]){
-        for(int i=g[bl[r]][0];i<=min(r,n);++i) a[i]+=c,a[i]%=mod;
+        for(int i=g[bl[r]][0];i<=g[bl[r]][1];++i){
+            a[i]=a[i]*cheng[bl[r]]+jia[bl[r]]+(l<=i&&i<=r?c:0);
+            a[i]%=mod;
+        }
+        cheng[bl[r]]=1;
+        jia[bl[r]]=0;
     }
     for(int i=bl[l]+1;i<=bl[r]-1;++i) jia[i]+=c,jia[i]%=mod;
 }
 inline void mul(int l,int r,int c){
-    for(int i=l;i<=min(g[bl[l]][1],r);++i) a[i]*=c,a[i]%=mod;
+    for(int i=g[bl[l]][0];i<=g[bl[l]][1];++i){
+        a[i]=((a[i]*cheng[bl[l]])%mod+jia[bl[l]]%mod)%mod*(l<=i&&i<=r?c:1);
+        a[i]%=mod;
+    }
+    cheng[bl[l]]=1;
+    jia[bl[l]]=0;
     if(bl[l]!=bl[r]){
-        for(int i=g[bl[r]][0];i<=min(n,r);++i) a[i]*=c,a[i]%=mod;
+        for(int i=g[bl[r]][0];i<=g[bl[r]][1];++i){
+            a[i]=((a[i]*cheng[bl[r]])%mod+jia[bl[r]]%mod)%mod*(l<=i&&i<=r?c:1);
+            a[i]%=mod;
+        }
+        cheng[bl[r]]=1;
+        jia[bl[r]]=0;
     }
     for(int i=bl[l]+1;i<=bl[r]-1;++i){
-        cheng[i]*=c,cheng[i]%=c;
-        jia[i]*=c,jia[i]%=c;
+        cheng[i]*=c,cheng[i]%=mod;
+        jia[i]*=c,jia[i]%=mod;
     }
 }
 int main()
@@ -61,8 +81,8 @@ int main()
     int op,l,r,c;
     for(int i=1;i<=n;++i){
         op=read();l=read();r=read();c=read();
-        if(op==0) add(l,r,c);
-        else if(op==1) mul(l,r,c);
+        if(op==0) add(l,r,c%mod);
+        else if(op==1) mul(l,r,c%mod);
         else write(((a[r]*cheng[bl[r]])%mod+jia[bl[r]]%mod)%mod),putchar('\n');
     }
     return 0;
