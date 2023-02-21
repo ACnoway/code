dde14c1176b35bb758302a9c5bc9b4bb24e71ccb
ACnoway <imlzhyt@163.com>
2023-02-21 20:58:43 +0800

    Luogu/P2154 [SDOI2009] 虔诚的墓主人

--- Luogu/P2154 [SDOI2009] 虔诚的墓主人.cpp
+++ Luogu/P2154 [SDOI2009] 虔诚的墓主人.cpp
@@ -21,10 +21,12 @@ inline int read(){
     }
     return x*f;
 }
-int n,m,w,k;
+const int maxn=100005;
+int n,m,w,k,cc;
+int tmp[maxn],cnt[maxn],C[maxn][12];
 struct cqs{
     int x,y;
-}a[100005];
+}a[maxn];
 bool cmp(cqs a,cqs b){
     if(a.x==b.x) return a.y<b.y;
     return a.x<b.x;
@@ -43,6 +45,16 @@ int main()
         a[i].y=read();
     }
     k=read();
-    
+    sort(a+1,a+w+1,cmp);
+    for(int i=0;i<=w;++i) C[i][0]=1;
+    for(int i=1;i<=w;++i){
+        for(int j=1;j<=min(i,k);++j)
+            C[i][j]=C[i-1][j]+C[i-1][j-1];
+    }
+    for(int i=1;i<=w;++i){
+        if(i==1||a[i].y!=a[i-1].y) cc++;
+        tmp[i]=cc;
+    }
+    for(int i=1;i<=w;++i) 
     return 0;
 }
