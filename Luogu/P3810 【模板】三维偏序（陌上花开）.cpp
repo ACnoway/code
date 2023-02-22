821d7233c150a07231c3937bb4044e095984b928
ACnoway <imlzhyt@163.com>
2023-02-22 21:02:20 +0800

    Luogu/P3810 【模板】三维偏序（陌上花开）

---
+++ Luogu/P3810 【模板】三维偏序（陌上花开）.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#else
+#define debug(x) cout<<' '<<#x<<'='<<x<<endl;
+#endif
+using namespace std;
+inline int read(){
+    int x=0,f=1;
+    char c=getchar();
+    while(c<'0'||c>'9'){
+        if(c=='-') f=-1;
+        c=getchar();
+    }
+    while(c>='0'&&c<='9'){
+        x=(x<<3)+(x<<1)+(c^48);
+        c=getchar();
+    }
+    return x*f;
+}
+inline void write(int x){
+    if(x<0){x=-x;putchar('-');}
+    if(x>9) write(x/10);
+    putchar(x%10+'0');
+}
+const int maxn=200005;
+struct node{
+    int a,b,c,cnt,ans;
+}s1[maxn],s2[maxn];
+int n,m,k,mx,top,su[maxn];
+int c[maxn];
+bool cmp1(node a,node b){
+    if(a.a!=b.a) return a.a<b.a;
+    if(a.b!=b.b) return a.b<b.b;
+    return a.c<b.c;
+}
+bool cmp2(node a,node b){
+    if(a.b!=b.b) return a.b<b.b;
+    return a.c<b.c;
+}
+inline int lowbit(int x){
+    return x&(-x);
+}
+void add(int x,int y){
+    while(x<=mx){
+        c[x]+=y;
+        x+=lowbit(x);
+    }
+}
+inline int query(int x){
+    int sum=0;
+    while(x){
+        sum+=c[x];
+        x-=lowbit(x);
+    }
+    return x;
+}
+void cdq(int l,int r){
+    if(l==r) return;
+    int mid=(l+r)>>1;
+    cdq(l,mid);
+    cdq(mid+1,r);
+    sort(s2+l,s2+mid+1,cmp2);
+    sort(s2+mid+1,s2+r+1,cmp2);
+    int i,j=l;
+    for(i=mid+1;i<=r;++i){
+        while(s2[i].b>=s2[j].b&&j<=mid){
+            add(s2[j].c,s2[j].cnt);
+            ++j;
+        }
+        s2[i].ans+=query(s2[i].c);
+    }//类似于归并的操作
+    //清空树状数组
+    for(i=l;i<j;++i) add(s2[i].c,-s2[i].cnt);
+}
+int main()
+{
+    n=read();
+    k=read();
+    mk=k;//树状数组的区间
+    for(int i=1;i<=n;++i){
+        s1[i].a=read();
+        s1[i]=b=read();
+        s1[i].c=read();
+    }
+    sort(s1+1,s1+1+n,cmp1);
+    for(int i=1;i<=n;++i){
+        top++;
+        if(s1[i].a!=)
+    }
+    return 0;
+}
