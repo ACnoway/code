c4ea4772624d017617a654cb0925116e59ecc839
ACnoway <imlzhyt@163.com>
2023-02-27 17:56:30 +0800

    何老师/B. 第 k 大查询

--- AtCoder/ABC 291/F - Teleporter and Closed off.cpp
+++ AtCoder/ABC 291/F - Teleporter and Closed off.cpp
@@ -26,17 +26,32 @@ inline void write(int x){
     if(x>9) write(x/10);
     putchar(x%10+'0');
 }
+const int maxn=100005,inf=1047483647;
 int n,m;
-int a[100005];
-string s[20];
+int qian[maxn],hou[maxn];
+char s[maxn][20];
 int main()
 {
     cin>>n>>m;
-    for(int i=1;i<=m;++i) cin>>s[i];
-    for(int i=2;i<n;++i){
-        for(int j=1;j<n;++j){
-            
+    for(int i=1;i<=m;++i) cin>>s[i]+1;
+    for(int i=0;i<=n+1;++i) qian[i]=hou[i]=inf;
+    qian[1]=0;hou[n]=0;
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=m;++j){
+            if(s[i][j]=='1'){
+                qian[i+j]=min(qian[i+j],qian[i]+1);
+            }
         }
+    }
+    for(int i=n;i;--i){
+        for(int j=1;j<=m;++j){
+            if(s[i][j]=='1'){
+                hou[i]=min(hou[i],hou[i+j]+1);
+            }
+        }
+    }
+    for(int i=2;i<n;++i){
+        for(int j=1;j<=)
     }
     return 0;
 }
