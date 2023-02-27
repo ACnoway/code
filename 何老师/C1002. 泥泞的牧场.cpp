fb241530e512fe8804bf10fb1dd17c932eb102fc
ACnoway <imlzhyt@163.com>
2023-02-27 13:37:01 +0800

    何老师/C1002. 泥泞的牧场

--- 何老师/C1002. 泥泞的牧场.cpp
+++ 何老师/C1002. 泥泞的牧场.cpp
@@ -26,56 +26,81 @@ inline void write(int x){
     if(x>9) write(x/10);
     putchar(x%10+'0');
 }
-int n,m;
-char a[60][60];
-bool v[60][60],fi[60][60];
-int dfs(int x,int y,int way,int cnt){
-    bool flag=1;
-    for(int i=1;i<=n;++i){
-        for(int j=1;j<=m;++j)
-            if(a[i][j]=='*'&&fi[i][j]==0){
-                flag=0;
-                break;
-            }
-        if(!flag) break;
-    }
-    if(flag) return cnt;
+const int maxn=3003;
+struct node{
+    int to,nxt;
+}e[maxn];
+int idx,head[maxn];
+inline void add(int u,int v){
+    e[++idx].to=v;
+    e[idx].nxt=head[u];
+    head[u]=idx;
 }
-int main()
-{
-    freopen("cover.in","r",stdin);
-    freopen("cover.out","w",stdout);
-    n=read();m=read();
-    for(int i=1;i<=n;++i) cin>>a[i]+1;
-    /*if(n<=5&&m<=5){
-        write(min(dfs(1,1,0,0),dfs(1,1,1,0)));
-        putchar('\n');
-        return 0;
-    }*/
-    int cnts=0,cnth=0,ans=0;
-    for(int i=1;i<=n;++i){
-        for(int j=1;j<=m;++j){
-            if(a[i][j]=='.') continue;
-            cnts=cnth=0;
-            for(int k=j-1;a[i][k]=='*';--k) cnth+=(!v[i][k]);
-            for(int k=j+1;a[i][k]=='*';++k) cnth+=(!v[i][k]);
-            for(int k=i-1;a[k][j]=='*';--k) cnts+=(!v[k][j]);
-            for(int k=i+1;a[k][j]=='*';++k) cnts+=(!v[k][j]);
-            if(cnth==0&&cnts==0) continue;
+
+int match[maxn],p[maxn];
+
+int n,m,ans,w,cnt;
+int lie[60],hang[60];
+char a[60][60];
+int top,st[maxn];
+
+bool dfs(int u,int q){
+    int v;
+    for(int i=head[u];i;i=e[i].nxt){
+        v=e[i].to;
+        if(v==q||p[v]==w) continue;
+        p[v]=w;
+        //如果对方没有被匹配
+        if(!match[v]){
             ans++;
-            if(cnth>cnts){
-                for(int k=j-1;a[i][k]=='*';--k) v[i][k]=1;
-                for(int k=j+1;a[i][k]=='*';++k) v[i][k]=1;
-            }
-            else{
-                for(int k=i-1;a[k][j]=='*';--k) v[k][j]=1;
-                for(int k=i+1;a[k][j]=='*';++k) v[k][j]=1;
+            match[v]=u;
+            return 1;
+        }
+        else{
+            //如果已经被匹配就让跟对方匹配的人换个人
+            if(dfs(match[v],v)){
+                match[v]=u;
+                return 1;
             }
         }
     }
+    return 0;
+}
+int main()
+{
+    n=read();
+    m=read();
+    for(int i=1;i<=n;++i) cin>>a[i]+1;
+    for(int i=0;i<=n+1;++i){
+        a[i][0]='.';
+        a[i][m+1]='.';
+    }
+    for(int i=0;i<=m+1;++i){
+        a[0][i]='.';
+        a[n+1][i]='.';
+    }
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=m;++j){
+            if(a[i][j]=='*'&&a[i-1][j]=='.'){
+                //新的一列
+                lie[j]=++cnt;
+                st[++top]=cnt;
+            }
+            if(a[i][j]=='*'&&a[i][j-1]=='.'){
+                //新的一行
+                hang[i]=++cnt;
+                //这里不用将cnt入栈是因为对于这个二分图我们从一个集合进入dfs就行，不用管另一个
+            }
+            if(a[i][j]=='*'){
+                add(lie[j],hang[i]);
+            }
+        }
+    }
+    for(int i=1;i<=top;++i){
+        w=i;
+        dfs(st[i],0);
+    }
     write(ans);
     putchar('\n');
-    fclose(stdin);
-    fclose(stdout);
     return 0;
 }
