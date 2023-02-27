731e3bbd7155ccfc6de9e1008a39a0a74451a7ad
ACnoway <imlzhyt@163.com>
2023-02-27 11:14:19 +0800

    AcWing/861. 二分图的最大匹配

--- a1test.cpp
+++ a1test.cpp
@@ -1,169 +1,63 @@
 #include<iostream>
-#include<algorithm>
-#define mod 1000000007
-#define int long long
+#include<cstring>
 using namespace std;
-int kd(){
-	int x=0,f=1;
-	char a=getchar();
-	while(a<'0'||a>'9'){
-		if(a=='-'){
-			f=-1;
-		} 
-		a=getchar();
-	}
-	while(a>='0'&&a<='9'){
-		x=x*10+a-'0';
-		a=getchar();
-	}
-	return x*f;
+const int N = 510 , M = 100010;
+int n1,n2,m;
+int h[N],ne[M],e[M],idx;
+bool st[N];
+int match[N];
+
+void add(int a , int b)
+{
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
 }
-int n;
-struct node{
-	int a,b;
-	int two,three;
-	int id;
-}r[100010];
-bool cnp(node x,node y){
-	return x.b==y.b?x.id<y.id:x.b<y.b;
+
+void init()
+{
+    memset(h,-1,sizeof h);
 }
-bool cmp(node x,node y){
-	return x.a<y.a;
+
+int find(int x)
+{
+    //遍历自己喜欢的女孩
+    for(int i = h[x] ; i != -1 ;i = ne[i])
+    {
+        int j = e[i];
+        if(!st[j])//如果在这一轮模拟匹配中,这个女孩尚未被预定
+        {
+            st[j] = true;//那x就预定这个女孩了
+            //如果女孩j没有男朋友，或者她原来的男朋友能够预定其它喜欢的女孩。配对成功
+            if(!match[j]||find(match[j]))
+            {
+                match[j] = x;
+                return true;
+            }
+
+        }
+    }
+    //自己中意的全部都被预定了。配对失败。
+    return false;
 }
-int ksm(int x,int y){
-	int ans=1;
-	while(y){
-		if(y%2==1){
-			ans=ans*x%mod;
-		}
-		x=x*x%mod;
-		y/=2;
-	}
-	return ans;
-}
-int ans=0;
-struct nod{
-	int l,r;
-	int sum;
-	int tot;
-	int lzj;
-}tree[400010];
-void push_down(int i){
-	tree[i*2].sum=tree[i*2].sum*tree[i].lzj%mod;
-	tree[i*2+1].sum=tree[i*2+1].sum*tree[i].lzj%mod;
-	tree[i*2].lzj=tree[i*2].lzj*tree[i].lzj%mod;
-	tree[i*2+1].lzj=tree[i*2+1].lzj*tree[i].lzj%mod;
-	tree[i].lzj=1;
-}
-void build(int i,int l,int r){
-	tree[i].l=l;
-	tree[i].r=r;
-	tree[i].lzj=1;
-	tree[i].sum=0;
-	tree[i].tot=0;
-	if(l==r){
-		return ;
-	}
-	int mid=(l+r)/2;
-	build(i*2,l,mid);
-	build(i*2+1,mid+1,r);
-	return ;
-}
-int searchtot(int i,int l,int r){
-	if(tree[i].l>=l&&tree[i].r<=r){
-		return tree[i].tot;
-	}
-	push_down(i);
-	int s=0;
-	if(tree[i*2].r>=l){
-		s+=searchtot(i*2,l,r);
-	}
-	if(tree[i*2+1].l<=r){
-		s+=searchtot(i*2+1,l,r);
-	}
-	return s;
-}
-void addsum(int i,int p,int k){
-	if(tree[i].l==tree[i].r){
-		tree[i].sum=k;
-		return ;
-	}
-	push_down(i);
-	if(tree[i*2].r>=p){
-		addsum(i*2,p,k);
-	}
-	else{
-		addsum(i*2+1,p,k);
-	}
-	tree[i].sum=(tree[i*2].sum+tree[i*2+1].sum)%mod;
-}
-int searchsum(int i,int l,int r){
-	if(tree[i].l>=l&&tree[i].r<=r){
-		return tree[i].sum;
-	}
-	push_down(i);
-	int s=0;
-	if(tree[i*2].r>=l){
-		s=(s+searchsum(i*2,l,r))%mod;
-	}
-	if(tree[i*2+1].l<=r){
-		s=(s+searchsum(i*2+1,l,r))%mod;
-	}
-	return s;
-}
-void addtot(int i,int p){
-	if(tree[i].l==tree[i].r){
-		tree[i].tot=1;
-		return ;
-	}
-	push_down(i);
-	if(tree[i*2].r>=p){
-		addtot(i*2,p);
-	}
-	else{
-		addtot(i*2+1,p);
-	}
-	tree[i].tot=tree[i*2].tot+tree[i*2+1].tot;
-}
-void cheng(int i,int l,int r){
-	if(l>r){
-		return ;
-	}
-	if(tree[i].l>=l&&tree[i].r<=r){
-		tree[i].sum=tree[i].sum*2%mod;
-		tree[i].lzj=tree[i].lzj*2%mod;
-		return ; 
-	}
-	push_down(i);
-	if(tree[i*2].r>=l){
-		cheng(i*2,l,r);
-	}
-	if(tree[i*2+1].l<=r){
-		cheng(i*2+1,l,r);
-	}
-	tree[i].sum=(tree[i*2].sum+tree[i*2+1].sum)%mod;
-}
-signed main(){
-	n=kd();
-	for(int i=1;i<=n;i++){
-		r[i].a=kd();r[i].b=kd();
-		r[i].two=ksm(2,r[i].a);
-		r[i].three=ksm(3,r[i].b);
-		r[i].id=i;
-	}
-	sort(r+1,r+n+1,cnp);
-	for(int i=1;i<=n;i++){
-		r[i].id=i;
-	}
-	sort(r+1,r+n+1,cmp);
-	build(1,1,n);
-	for(int i=1;i<=n;i++){
-		int cnt=searchtot(1,1,r[i].id);
-		addsum(1,r[i].id,ksm(2,cnt)*r[i].three%mod);
-		ans=(ans+searchsum(1,r[i].id,n)*r[i].two%mod)%mod;
-		cheng(1,r[i].id+1,n);
-		addtot(1,r[i].id);
-	}
-	cout<<ans;
-	return 0;
+int main()
+{
+    init();
+    cin>>n1>>n2>>m;
+    while(m--)
+    {
+        int a,b;
+        cin>>a>>b;
+        add(a,b);
+    }
+
+
+    int res = 0;
+    for(int i = 1; i <= n1 ;i ++)
+    {  
+         //因为每次模拟匹配的预定情况都是不一样的所以每轮模拟都要初始化
+          memset(st,false,sizeof st);
+        if(find(i)) 
+          res++;
+    }  
+
+    cout<<res<<endl;
 }
