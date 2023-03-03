ad1cbee28133875e79bcdcc57de9f22115c307ce
ACnoway <imlzhyt@163.com>
2023-03-03 09:48:16 +0800

    何老师/C1024. 异只因或

--- 何老师/C1024. 异只因或.cpp
+++ 何老师/C1024. 异只因或.cpp
@@ -3,6 +3,7 @@
 #include<algorithm>
 #include<cmath>
 #include<vector>
+#define int long long
 #ifdef ONLINE_JUDGE
 #define debug(x)
 #else
@@ -27,14 +28,44 @@ inline void write(int x){
     if(x>9) write(x/10);
     putchar(x%10+'0');
 }
-const int maxn=500005;
+//注意trie树的宽度，这里因为最多是2的30次方所以要乘以30
+const int maxn=500005*31;
 int ch[maxn][2];
-int val[maxn],budui[maxn];
+int bits[40][5];
+int siz[maxn],budui[maxn];
 int n,cnt,a,ans;
-vector<int> bit[maxn];
-int main()
+void insert(int x){
+    int now=1,d;
+    ++siz[now];
+    for(int i=29;i>=0;--i){
+        d=(x>>i)&1ll;
+        //如果没孩子就新建个孩子
+        if(!ch[now][d]){
+            ch[now][d]=++cnt;
+        }
+        //转到孩子
+        now=ch[now][d];
+        //统计违法的方案个数，加上这一位之前已经插入的个数
+        budui[now]+=bits[i][d];
+        ++bits[i][d];
+        ++siz[now];
+    }
+}
+int query(int x){
+    int sum=0,tmp,now=1,d;
+    for(int i=29;i>=0;--i){
+        d=(x>>i)&1ll;
+        tmp=siz[ch[now][!d]]*bits[i][!d]-siz[ch[now][!d]]-budui[ch[now][!d]];
+        sum+=tmp;
+        now=ch[now][d];
+        if(!now) break;
+    }
+    return sum;
+}
+signed main()
 {
     n=read();
+    cnt=1;
     for(int i=1;i<=n;++i){
         a=read();
         ans+=query(a);
