fd0f0d4d0d3b5957b8212ea4be48f3fb8e50a9df
ACnoway <imlzhyt@163.com>
2023-03-04 20:48:36 +0800

    何老师/C1010. 路径查询

--- 何老师/C1010. 路径查询.cpp
+++ 何老师/C1010. 路径查询.cpp
@@ -4,6 +4,7 @@
 #include<cmath>
 #include<set>
 #include<array>
+#include<vector>
 #ifdef ONLINE_JUDGE
 #define debug(x)
 #else
@@ -33,9 +34,9 @@ int n,m,q;
 int p[maxn],ans[maxn];
 struct node{
     int u,v,w;
-}bian[maxn];
+}bian[maxn<<2];
 set<int> e[maxn];
-set<array<int,2>> qs[maxn];
+set<array<int,2> > qs[maxn];
 bool cmp(node a,node b){
     return a.w<b.w;
 }
@@ -47,9 +48,40 @@ void work(int x,int y,int val){
     for(auto u:e[x]){
         while(1){
             auto it=qs[y].lower_bound({u,-1});
-            if(it==qs[y].end()||)
+            if(it==qs[y].end()||(*it)[0]!=u) break;
+            int id=(*it)[1];
+            ans[id]=val;
+            qs[y].erase({u,id});
+            qs[u].erase({y,id});
         }
     }
+    vector<array<int,3> > shan;
+    for(auto u:qs[x]){
+        if(e[y].count(u[0])){
+            ans[u[1]]=val;
+            shan.push_back({x,u[0],u[1]});
+        }
+    }
+    for(auto u:shan){
+        qs[u[0]].erase({u[1],u[2]});
+        qs[u[1]].erase({u[0],u[2]});
+    }
+    shan.clear();
+    for(auto u:e[x]){
+        if(u!=y){
+            e[y].insert(u);
+            e[u].insert(y);
+        }
+        e[u].erase(x);
+    }
+    e[x].clear();
+    for(auto u:qs[x]){
+        qs[y].insert(u);
+        qs[u[0]].insert({y,u[1]});
+        qs[u[0]].erase({x,u[1]});
+    }
+    qs[x].clear();
+    p[x]=y;
 }
 int main()
 {
