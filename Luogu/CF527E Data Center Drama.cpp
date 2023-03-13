04c549b6d087f80035a2847d3b3cd8bc7cac14c0
ACnoway <imlzhyt@163.com>
2023-03-13 11:02:00 +0800

    Luogu/CF527E Data Center Drama

---
+++ Luogu/CF527E Data Center Drama.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#else
+#define debug(x) cout<<' '<<#x<<'='<<x<<endl
+#endif
+using namespace std;
+inline int read(){
+    int x=0,f=1;
+    char c=getchar();
+    while(c<'0'||c>'9'){
+        if(c=='-') f=-1;
+        c=getchar();
+    }
+    while(c>='0'&&c<='9'){
+        x=(x<<3)+(x<<1)+(c^48);
+        c=getchar();
+    }
+    return x*f;
+}
+const int N=200005,M=1000006;
+int n,m,cnt;
+int du[N];
+bool vis[M];
+vector<int> ji;
+struct node{
+    int to,nxt;
+}e[M];
+int idx=1,head[N];
+inline void add(int u,int v){
+    e[++idx].to=v;
+    e[idx].nxt=head[u];
+    head[u]=idx;
+}
+void dfs(int x){
+    //因为要删边所以i要加取地址符更改head[x]
+    for(int &i=head[x];i;i=e[i].nxt){
+        if(vis[i]) continue;
+        vis[i]=vis[i^1]=1;
+        int y=e[i].to;
+        dfs(y);
+        ++cnt;
+        if(cnt&1) printf("%d %d\n",x,y);
+        else printf("%d %d\n",y,x);
+    }
+}
+int main()
+{
+    n=read(); m=read();
+    int x,y;
+    for(int i=1;i<=m;++i){
+        x=read(); y=read();
+        ++du[x]; ++du[y];
+        add(x,y); add(y,x);
+    }
+    for(int i=1;i<=n;++i){
+        if(du[i]&1) ji.push_back(i);
+    }
+    for(int i=0;i<ji.size();i+=2){
+        add(ji[i],ji[i+1]);
+        ++m;
+    }
+    if(m&1){
+        add(1,1);
+        ++m;
+    }
+    printf("%d\n",m);
+    dfs(1);
+    return 0;
+}
