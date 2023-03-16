925ce3452dc7c6dc7e554d0298ab4da0cebb8e3f
ACnoway <imlzhyt@163.com>
2023-03-16 23:19:07 +0800

    Luogu/P4334 [COI2007] Policija

--- Luogu/P4334 [COI2007] Policija.cpp
+++ Luogu/P4334 [COI2007] Policija.cpp
@@ -3,6 +3,7 @@
 #include<algorithm>
 #include<cmath>
 #include<vector>
+#define int long long
 #ifdef ONLINE_JUDGE
 #define debug(x)
 #else
@@ -22,11 +23,84 @@ inline int read(){
     }
     return x*f;
 }
-const int N=100005,M=1000006;
+const int N=500005;
 int n,m;
-vector<int> e[N],E[N];
-int dfid,dfn[N],cdfn[N],low[N];
-int main()
+vector<int> e[N];
+int dfid,dfn[N],hui[N],low[N],dep[N];
+int fa[N][21];
+void tarjan(int x,int die){
+    dep[x]=dep[die]+1;
+    fa[x][0]=die;
+    int k=__lg(dep[x])+1;
+    for(int i=1;i<=k;++i) fa[x][i]=fa[fa[x][i-1]][i-1];
+    dfn[x]=low[x]=++dfid;
+    for(int v:e[x]){
+        if(!dfn[v]){
+            tarjan(v,x);
+            low[x]=min(low[x],low[v]);
+        }
+        else if(v!=die) low[x]=min(low[x],dfn[v]);
+    }
+    hui[x]=++dfid;
+}
+int lca(int x,int y){
+    if(dep[x]<dep[y]) swap(x,y);
+    while(dep[x]>dep[y]) x=fa[x][__lg(dep[x]-dep[y])];
+    if(x==y) return x;
+    for(int i=__lg(dep[x])+1;i>=0;--i){
+        if(fa[x][i]!=fa[y][i]){
+            x=fa[x][i];
+            y=fa[y][i];
+        }
+    }
+    return fa[x][0];
+}
+bool baohan(int x,int y){
+    return dfn[x]<=dfn[y]&&hui[x]>=hui[y];
+}
+int xiayige(int x,int y){
+    //找x与y的路径中x的下一个点
+    int dx=dep[x]+1;
+    while(dep[y]>dx) y=fa[y][__lg(dep[y]-dx)];
+    return y;
+}
+bool query1(int a,int b,int g1,int g2){
+    if(a==b) return 1;
+    if(dfn[a]>dfn[b]) swap(a,b);
+    if(dfn[g1]>dfn[g2]) swap(g1,g2);
+    if(dep[g2]!=dep[g1]+1) return 1;//g1到g2是回边
+    debug(a);
+    if(baohan(a,b)){
+        if(baohan(a,g1)&&baohan(g2,b)){
+            return low[g2]<=dfn[g1];//判割边
+        }
+        else return 1;
+    }
+    else{
+        int rt=lca(a,b);
+        return query1(a,rt,g1,g2)&&query1(b,rt,g1,g2);
+    }
+}
+bool query2(int a,int b,int c){
+    if(a==b) return a!=c;
+    if(a==c||b==c) return 0;
+    if(dfn[a]>dfn[b]) swap(a,b);
+    if(baohan(a,b)){
+        if(baohan(a,c)&&baohan(c,b)){
+            //找割点
+            return low[xiayige(c,b)]<dfn[c];
+        }
+        else return 1;
+    }
+    else{
+        int rt=lca(a,b);
+        if(baohan(rt,c)&&(baohan(c,a)||baohan(c,b))){
+            return query2(a,fa[rt][0],c)&&query2(b,fa[rt][0],c);
+        }
+        else return 1;
+    }
+}
+signed main()
 {
     n=read(); m=read();
     for(int i=1;i<=m;++i){
@@ -35,5 +109,20 @@ int main()
         e[y].push_back(x);
     }
     tarjan(1,1);
+    int q,a,b,x,y;
+    q=read();
+    for(int i=1;i<=q;++i){
+        a=read();
+        if(a==1){
+            a=read(); b=read();
+            x=read(); y=read();
+            printf((query1(a,b,x,y)?"yes\n":"no\n"));
+        }
+        else{
+            a=read(); b=read();
+            x=read();
+            printf((query2(a,b,x)?"yes\n":"no\n"));
+        }
+    }
     return 0;
 }
--- a1test.cpp
+++ a1test.cpp
@@ -1,14 +1,151 @@
-#include<bits/stdc++.h>
+// 请使用C++11编译
+#include <bits/stdc++.h>
 using namespace std;
+constexpr int N = 1000001;
+struct edge
+{
+    int to, next;
+};
+int head[N];
+vector<edge> E;
+void insert(int u, int v)
+{
+    E.push_back({v, head[u]});
+    head[u] = E.size() - 1;
+}
+int dfn[N], finish[N], low[N], now;
+int d[N], fa[N][20], lg[N];
+void tarjan(int u, int f)
+{
+    d[u] = d[f] + 1;
+    fa[u][0] = f;
+    for (int i = 1; i <= lg[d[u]]; i++)
+        fa[u][i] = fa[fa[u][i - 1]][i - 1];
+    dfn[u] = low[u] = ++now;
+    for (int i = head[u]; ~i; i = E[i].next)
+    {
+        int v = E[i].to;
+        if (!dfn[v])
+        {
+            tarjan(v, u);
+            low[u] = min(low[u], low[v]);
+        }
+        else if (v != f)
+            low[u] = min(low[u], dfn[v]);
+    }
+    finish[u] = ++now;
+}
+int lca(int x, int y)
+{
+    if (d[x] < d[y])
+        swap(x, y);
+    while (d[x] > d[y])
+        x = fa[x][lg[d[x] - d[y]] - 1];
+    if (x == y)
+        return x;
+    for (int i = lg[d[x]] - 1; i >= 0; i--)
+    {
+        if (fa[x][i] != fa[y][i])
+            x = fa[x][i], y = fa[y][i];
+    }
+    return fa[x][0];
+}
+int n, m, q;
+
+// 判断节点 f 是否包含节点 a
+bool contains(int f, int a)
+{
+    return dfn[f] <= dfn[a] && finish[f] >= finish[a];
+}
+
+// 查找 f 到 a （ f 包含 a ）的路径上，与 f 直接相连的点
+int find_related_child(int f, int a)
+{
+    int df = d[f] + 1;
+    while (d[a] > df)
+        a = fa[a][lg[d[a] - df] - 1];
+    return a;
+}
+bool query1(int a, int b, int g1, int g2)
+{
+    if (a == b)
+        return true;
+    if (dfn[a] > dfn[b])
+        swap(a, b);
+    if (dfn[g1] > dfn[g2])
+        swap(g1, g2);
+    if (d[g2] != d[g1] + 1)
+        return true;    // g1 -> g2 为回边
+    if (contains(a, b)) // a 包含 b
+    {
+        if (contains(a, g1) && contains(g2, b))
+        {
+            return low[g2] <= dfn[g1];
+        }
+        else
+            return true;
+    }
+    else
+    {
+        int l = lca(a, b);
+        return query1(a, l, g1, g2) && query1(b, l, g1, g2);
+    }
+}
+bool query2(int a, int b, int c)
+{
+    if (a == b)
+        return a != c;
+    if (a == c || b == c)
+        return false;
+    if (dfn[a] > dfn[b])
+        swap(a, b);
+    if (contains(a, b)) // a 包含 b
+    {
+        if (contains(a, c) && contains(c, b))
+        {
+            return low[find_related_child(c, b)] < dfn[c]; // 注意这里是小于
+        }
+        else
+            return true;
+    }
+    else
+    {
+        int l = lca(a, b);
+        if (contains(l, c) && (contains(c, a) || contains(c, b)))
+        {
+            return query2(a, fa[l][0], c) && query2(b, fa[l][0], c);
+        }
+        else
+            return true;
+    }
+}
 int main()
 {
-    int a;
-    string s="";
-    cin>>a;
-    while(a){
-        s=char((a&1)+'0')+s;
-        a>>=1;
+    memset(head, -1, sizeof head);
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        lg[i] = lg[i - 1] + (1 << lg[i - 1] == i);
+    for (int i = 1, u, v; i <= m; i++)
+    {
+        cin >> u >> v;
+        insert(u, v);
+        insert(v, u);
     }
-    cout<<s<<endl;
+    tarjan(1, 1);
+    cin >> q;
+    for (int i = 1, a, b, c, d; i <= q; i++)
+    {
+        cin >> a;
+        if (a == 1)
+        {
+            cin >> a >> b >> c >> d;
+            cout << (query1(a, b, c, d) ? "yes" : "no") << endl;
+        }
+        else
+        {
+            cin >> a >> b >> c;
+            cout << (query2(a, b, c) ? "yes" : "no") << endl;
+        }
+    }
     return 0;
 }
