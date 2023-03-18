9ba4bcaf4996d85e77198bb771aa2d22e2f84065
ACnoway <imlzhyt@163.com>
2023-03-18 10:31:24 +0800

    AtCoder\ABC 293/A - Swap Odd and Even

---
+++ AtCoder/ABC 293/A - Swap Odd and Even.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#else
+#define debug(x) cout<<' '<<#x<<'='<<x<<endl
+#endif
+using namespace std;
+inline int read(){
+    int x=0,f=1;
+    char c=getchar();
+    while(c<'0'||c>'9'){
+        if(c=='-') f=-1;
+        c=getchar();
+    }
+    while(c>='0'&&c<='9'){
+        x=(x<<3)+(x<<1)+(c^48);
+        c=getchar();
+    }
+    return x*f;
+}
+string s;
+int main()
+{
+    cin>>s;
+    for(int i=1;i<s.size();i+=2){
+        putchar(s[i]);
+        putchar(s[i-1]);
+    }
+    return 0;
+}
