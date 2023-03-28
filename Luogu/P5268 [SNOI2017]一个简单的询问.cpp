58a6e6702313bef9bdcf644c75067747bbfa858f
ACnoway <imlzhyt@163.com>
2023-03-28 16:33:14 +0800

    Luogu/P5268 [SNOI2017]一个简单的询问

--- Luogu/P5268 [SNOI2017]一个简单的询问.cpp
+++ Luogu/P5268 [SNOI2017]一个简单的询问.cpp
@@ -22,8 +22,8 @@ inline int read(){
     return x*f;
 }
 const int N=50004;
-int n,m;
-int a[N],bl[N],g[N][2];
+int n,m,len,siz,qc,sum;
+int a[N],bl[N],g[N][2],ans[N],cntl[N],cntr[N];
 struct wenti{
     int l,r,id,d;
     bool operator <(const wenti &x)const{
@@ -33,10 +33,37 @@ struct wenti{
 int main()
 {
     n=read();
+    len=sqrt(n);
+    siz=n/len;
+    if(len*siz<n) ++siz;
     for(int i=1;i<=n;++i) a[i]=read();
+    for(int i=1;i<=siz;++i){
+        g[i][0]=(i-1)*len+1;
+        g[i][1]=min(n,i*len);
+        for(int j=g[i][0];j<=g[i][1];++j) bl[j]=i;
+    }
     m=read();
     for(int i=1;i<=m;++i){
-        a[]
+        int l1=read(),r1=read(),l2=read(),r2=read();
+        q[++qc]={r1,r2,i,1};
+        q[++qc]={r1,l2-1,i,-1};
+        q[++qc]={l1-1,r2,i,-1};
+        q[++qc]={l1-1,l2-1,i,1};
+    }
+    for(int i=1;i<=qc;++i){
+        if(q[i].l>q[i].r) swap(q[i].l,q[i].r);
+    }
+    sort(q+1,q+qc+1);
+    int l=0,r=0;
+    for(auto i:q){
+        while(l<i.l) sum+=cntr[a[++l]],++cntl[a[l]];
+        while(l>i.l) sum-=cntr[a[l]],--cntl[a[l--]];
+        while(r<i.r) sum+=cntl[a[++r]],++cntr[a[r]];
+        while(r>i.r) sum-=cntl[a[r]],--cntr[a[r--]];
+        ans[i.id]+=sum*i.d;
+    }
+    for(int i=1;i<=m;++i){
+        printf("%d\n",ans[i]);
     }
     return 0;
 }
