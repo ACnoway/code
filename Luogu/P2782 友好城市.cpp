16f01dfd7b6bac2ea44ba1e701b1be518b671a68
ACnoway <imlzhyt@163.com>
2023-04-11 18:38:53 +0800

    Luogu/P2782 友好城市

--- Luogu/P2782 友好城市.cpp
+++ Luogu/P2782 友好城市.cpp
@@ -8,22 +8,20 @@ bool cmp(node a,node b){
     return a.a<b.a;
 }
 int f[200005];
-int n,x,y;
+int n,len;
 int main()
 {
-    cin>>x>>y>>n;
+    cin>>n;
     for(int i=1;i<=n;++i){
         cin>>t[i].a>>t[i].b;
     }
     sort(t+1,t+n+1,cmp);
-    int ans=0;
-    for(int i=1;i<=n;++i){
-        f[i]=1;
-        for(int j=1;j<i;++j){
-            if(t[i].b>t[j].b) f[i]=max(f[i],f[j]+1);
-        }
-        ans=max(ans,f[i]);
+    f[++len]=t[1].a;
+    for(int i=2;i<=n;++i){
+        int d=upper_bound(f+1,f+len+1,t[i].a)-f;
+        f[d]=t[i].a;
+        if(d>len) ++len;
     }
-    cout<<ans<<endl;
+    cout<<len<<endl;
     return 0;
 }
