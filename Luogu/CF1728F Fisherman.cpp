b900b6ad8b4c87718c5f65b51fb726a0cdc720b5
ACnoway <imlzhyt@163.com>
2023-04-12 18:50:27 +0800

    Luogu/CF1728F Fisherman

--- Luogu/CF1728F Fisherman.cpp
+++ Luogu/CF1728F Fisherman.cpp
@@ -25,8 +25,20 @@ inline int read(){
 const int N=1003;
 int n,ma;
 int a[N],match[N];
+bool vis[N];
 vector<int> b;
 vector<int> e[N*N];
+bool dfs(int x){
+    for(int i:e[x]){
+        if(vis[i]) continue;
+        vis[i]=1;
+        if(match[i]==-1||dfs(match[i])){
+            match[i]=x;
+            return 1;
+        }
+    }
+    return 0;
+}
 int main()
 {
     n=read();
@@ -37,9 +49,21 @@ int main()
             b.push_back(a[i]*j);
         }
     }
-    ma=ma*ma;
-    for(int i=1;i<=ma;++i){
-        
+    sort(b.begin(),b.end());
+    b.erase(unique(b.begin(),b.end()),b.end());
+    //一个小trick，unique返回的是去重后的end()，然后在原数组中将新end()到旧end()的位置全部删除
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=n;++j){
+            e[lower_bound(b.begin(),b.end(),a[i]*j)-b.begin()].push_back(i);
+        }
     }
+    long long ans=0;
+    for(int i=0;i<b.size();++i){
+        if(dfs(i)){
+            ans+=b[i];
+            for(int j=0;j<=n;++j) vis[j]=0;
+        }
+    }
+    printf("%lld\n",ans);
     return 0;
 }
