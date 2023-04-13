944aa84670ae5d06b5c8a6b01da306e4b88548a2
ACnoway <imlzhyt@163.com>
2023-04-13 20:44:22 +0800

    何老师/#C. 可爱序列

--- a1test.cpp
+++ a1test.cpp
@@ -1,144 +1,33 @@
-#include <cstdio>
-#include <algorithm>
 #include <iostream>
-#include <cstring>
-#define INF 0x3e3e3e3e
-#include <queue>
-#define MAXN 100010
+#define int long long
 using namespace std;
-struct Edge
+int ans, n, a[45], f[45][45][2345][2];
+signed main()
 {
-    int to;
-    int nxt;
-    int cup;
-    int flow;
-} edge[MAXN << 1];
-int deep[MAXN];
-int head[MAXN];
-int cnt = 1, ans = 0;
-int n, m, s, t;
-queue<int> q;
-int read()
-{
-    int nm = 0, f = 1;
-    char c = getchar();
-    for (; !isdigit(c); c = getchar())
-        if (c == '-')
-            f = -1;
-    for (; isdigit(c); c = getchar())
-        nm = nm * 10 + c - '0';
-    return nm * f;
-}
-void push(int vi, int vj, int wei)
-{
-    cnt++;
-    edge[cnt].to = vj;
-    edge[cnt].cup = wei;
-    edge[cnt].nxt = head[vi];
-    head[vi] = cnt;
-    cnt++;
-    edge[cnt].to = vi;
-    edge[cnt].nxt = head[vj];
-    head[vj] = cnt;
-}
-bool bfs(int be, int ed)
-{
-    while (!q.empty())
-        q.pop();
-    memset(deep, 0, sizeof(deep));
-    deep[be] = 1;
-    q.push(be);
-    while (!q.empty())
-    {
-        int op = q.front();
-        q.pop();
-        for (int i = head[op]; i; i = edge[i].nxt)
-        {
-            int vj = edge[i].to;
-            if (deep[vj] || edge[i].cup <= edge[i].flow)
-                continue;
-            deep[vj] = deep[op] + 1;
-            q.push(vj);
-            if (vj == ed)
-                return true;
-        }
-    }
-    return false;
-}
-int dfs(int now, int ed, int flow)
-{
-    if (flow == 0 || now == ed)
-        return flow;
-    int tot = 0, f;
-    for (int i = head[now]; i; i = edge[i].nxt)
-    {
-        int vj = edge[i].to;
-        if (deep[vj] != deep[now] + 1)
-            continue;
-        int op = min(edge[i].cup - edge[i].flow, flow);
-        if (f = dfs(vj, ed, op))
-        {
-            edge[i].flow += f;
-            edge[i ^ 1].flow -= f;
-            tot += f;
-            flow -= f;
-        }
-        if (flow == 0)
-            break;
-    }
-    if (tot == 0)
-        deep[now] = 0;
-    return tot;
-}
-void Dinic(int be, int ed)
-{
-    while (bfs(be, ed))
-        ans -= dfs(be, ed, INF);
-}
-int main()
-{
-    n = read();
-    m = read();
-    s = n + m + 1;
-    t = s + 1;
+    cin >> n;
     for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= m; j++)
-        {
-            push(i, j + n, 1);
-        }
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        int op = read();
-        push(s, i, op);
-        ans += op;
-    }
-    for (int j = 1; j <= m; j++)
-    {
-        int op = read();
-        push(j + n, t, op);
-    }
-    Dinic(s, t);
-    if (ans == 0)
-    {
-        printf("1\n");
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = head[i]; j; j = edge[j].nxt)
-            {
-                int vj = edge[j].to;
-                if (vj != s && edge[j].flow)
-                {
-                    printf("%d ", edge[j].to - n);
-                }
-            }
-            printf("\n");
-        }
-    }
+        cin >> a[i];
+    if (a[1] != -1)
+        f[1][a[1]][a[1]][0] = 1;
     else
+        for (int i = 0; i <= 40; i++)
+            f[1][i][i][0] = 1;
+    for (int i = 2; i <= n; i++)
     {
-        printf("0\n");
+        int l = 0, r = 40;
+        if (a[i] != -1)
+            l = r = a[i];
+        for (int o = l; o <= r; o++)
+            for (int j = o * (i - 1); j < 2023; j++)
+            {
+                for (int k = 0; k <= o; k++)
+                    f[i][o][j + o][0] = (f[i][o][j + o][0] + f[i - 1][k][j][0] + f[i - 1][k][j][1]) % 1000000007;
+                for (int k = o + 1; k <= 40; k++)
+                    f[i][o][j + o][1] = (f[i][o][j + o][1] + f[i - 1][k][j][0]) % 1000000007;
+            }
     }
-    return 0;
+    for (int i = 0; i <= 40; i++)
+        for (int j = 0; j <= 40 * n; j++)
+            ans = (ans + f[n][i][j][0] + f[n][i][j][1]) % 1000000007;
+    cout << ans;
 }
--- 何老师/#C. 可爱序列.cpp
+++ 何老师/#C. 可爱序列.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#define int long long
 #ifdef ONLINE_JUDGE
 #define debug(x)
 #else
@@ -18,18 +19,34 @@ inline int read(){
     }
     return x*f;
 }
-const int N=50,mod=1e9+7;
-int n;
+const int N=50,mod=1000000007;
+int ans,n;
 int a[N];
-int f[N][N][N];
-int main()
+int f[N][N][N*N][2];
+signed main()
 {
     n=read();
     for(int i=1;i<=n;++i) a[i]=read();
-    for(int i=)
-    for(int i=1;i<=n;++i){
-        
+    if(a[1]!=-1) f[1][a[1]][a[1]][0]=1;
+    else for(int i=0;i<=40;++i) f[1][i][i][0]=1;
+    for(int i=2;i<=n;++i){
+        int l=0,r=40;
+        if(a[i]!=-1) l=r=a[i];
+        for(int p=l;p<=r;++p)
+            //如果a[i]是-1就枚举它的可能性
+            for(int j=p*(i-1);j<=2023;++j){
+                for(int k=0;k<=p;++k)
+                    f[i][p][j+p][0]=(f[i][p][j+p][0]+f[i-1][k][j][0]+f[i-1][k][j][1])%mod;
+                for(int k=p+1;k<=40;++k)
+                    f[i][p][j+p][1]=(f[i][p][j+p][1]+f[i-1][k][j][0])%mod;
+            }
     }
+    for(int i=0;i<=40;++i){
+        for(int j=0;j<=40*n;++j){
+            ans=(ans+f[n][i][j][0]+f[n][i][j][1])%mod;
+        }
+    }
+    printf("%d\n",ans);
     return 0;
 }
 //%%%%%%%% stO zyb txdy Orz %%%%%%%%
