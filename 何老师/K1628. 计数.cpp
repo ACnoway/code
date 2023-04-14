1ce31f07587152627a67a97f54fc89318059fed5
ACnoway <imlzhyt@163.com>
2023-04-14 10:22:48 +0800

    何老师/K1628. 计数

--- 何老师/K1628. 计数.cpp
+++ 何老师/K1628. 计数.cpp
@@ -24,15 +24,22 @@ inline int read(){
 }
 const int N=1e7+10,mod=1e9+7;
 int n,m,ans,tmp;
-int a[20],jc[N];
+int a[N],jc[N],inv[N];
 signed main()
 {
     n=read();
     m=read();
+    inv[1]=1;
+    for(int i=2;i<=n;++i) inv[i]=(mod-mod/i)*inv[mod%i]%mod;
     jc[0]=1;
     for(int i=1;i<=n;++i) jc[i]=(jc[i-1]*i)%mod;
     for(int i=1;i<=m;++i){
-        
+        a[i]=1;
     }
+    a[m+1]=m*inv[m+1]%mod;
+    for(int i=m+2;i<=n;++i){
+        a[i]=((a[i-1]*(i-1)%mod+a[i-1]-a[i-m-1])%mod+mod)%mod*inv[i]%mod;
+    }
+    printf("%lld\n",a[n]*jc[n]%mod);
     return 0;
 }
