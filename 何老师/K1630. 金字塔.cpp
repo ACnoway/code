85b4e24947777b4a9a1cbc8c50baf09276410279
ACnoway <imlzhyt@163.com>
2023-04-14 10:28:00 +0800

    何老师/K1630. 金字塔

--- 何老师/K1630. 金字塔.cpp
+++ 何老师/K1630. 金字塔.cpp
@@ -21,6 +21,14 @@ inline int read(){
 int T,l,n,now,cnt;
 //* 1黑0白
 bool a[2][602][602];
+bool C(int i,int j){
+    while(i&&j){
+        if(!(i&1)&&(j&1)) return 0;
+        i>>=1;
+        j>>=1;
+    }
+    return 1;
+}
 int main()
 {
     srand(time(0)+603);
@@ -28,184 +36,18 @@ int main()
     while(T--){
         l=read(); n=read();
         if(n==0){
-            if(l&1) printf("Black\n");
-            else printf("White\n");
+            if(l&1) printf("White\n");
+            else printf("Black\n");
             continue;
         }
-        if(l>600){
-            for(int i=1;i<=n;++i){
-                int x=read(),y=read();
-            }
-            if(rand()%2) printf("White\n");
-            else printf("Black\n");
-        }
+        int ans=0;
         for(int i=1;i<=n;++i){
             int x=read(),y=read();
-            a[0][x][y]=1;
+            ans^=(C(l-1,x-1)*C(x-1,y-1));
         }
-        now=1;
-        for(int m=l-1;m;--m){
-            for(int i=1;i<=m;++i){
-                for(int j=1;j<=i;++j){
-                    cnt=a[now^1][i][j]+a[now^1][i+1][j]+a[now^1][i+1][j+1];
-                    if(cnt==2||cnt==0) a[now][i][j]=1;
-                    else a[now][i][j]=0;
-                }
-            }
-            now^=1;
-        }
-        if(a[now^1][1][1]) printf("Black\n");
+        ans^=(l&1)^1;
+        if(ans) printf("Black\n");
         else printf("White\n");
     }
     return 0;
 }
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO zyb txdy Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO xhx txde Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
-//%%%%%%%% stO lxy txds Orz %%%%%%%%
