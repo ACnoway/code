53192842f072498f7b0ed516c6a8416b0b5474e2
ACnoway <imlzhyt@163.com>
2023-04-22 16:39:37 +0800

    Luogu/CF1110E Magic Stones

--- Luogu/CF1110E Magic Stones.cpp
+++ Luogu/CF1110E Magic Stones.cpp
@@ -26,28 +26,25 @@ int T,n;
 int a[N],b[N],fa[N],fb[N];
 int main()
 {
-    T=read();
-    while(T--){
-        n=read();
-        for(int i=1;i<=n;++i) a[i]=read();
-        for(int i=1;i<=n;++i) b[i]=read();
-        if(a[1]!=b[1]||a[n]!=b[n]){
-            cout<<"No"<<endl;
-            continue;
-        }
-        for(int i=1;i<n;++i) fa[i]=a[i+1]-a[i];
-        for(int i=1;i<n;++i) fb[i]=b[i+1]-b[i];
-        sort(fa+1,fa+n);
-        sort(fb+1,fb+n);
-        bool flag=1;
-        for(int i=1;i<n;++i){
-            if(fa[i]!=fb[i]){
-                flag=0;
-                break;
-            }
-        }
-        if(!flag) cout<<"No"<<endl;
-        else cout<<"Yes"<<endl;
+    n=read();
+    for(int i=1;i<=n;++i) a[i]=read();
+    for(int i=1;i<=n;++i) b[i]=read();
+    if(a[1]!=b[1]||a[n]!=b[n]){
+        cout<<"No"<<endl;
+        return 0;
     }
+    for(int i=1;i<n;++i) fa[i]=a[i+1]-a[i];
+    for(int i=1;i<n;++i) fb[i]=b[i+1]-b[i];
+    sort(fa+1,fa+n);
+    sort(fb+1,fb+n);
+    bool flag=1;
+    for(int i=1;i<n;++i){
+        if(fa[i]!=fb[i]){
+            flag=0;
+            break;
+        }
+    }
+    if(!flag) cout<<"No"<<endl;
+    else cout<<"Yes"<<endl;
     return 0;
 }
