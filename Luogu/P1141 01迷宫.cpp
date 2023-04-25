99d6c6b230dfc3c96e04270201ec8be3d621acc0
ACnoway <imlzhyt@163.com>
2023-04-25 22:33:42 +0800

    Luogu/P1141 01迷宫

--- Luogu/P1141 01迷宫.cpp
+++ Luogu/P1141 01迷宫.cpp
@@ -10,8 +10,8 @@
 #endif
 using namespace std;
 int n,m,cnt;
-int a[1003][1003],b[1003][1003];
-int an[1000006];
+int a[1003][1003],vis[1003][1003];
+int ans[1000006];
 int dx[4]={1,0,-1,0},dy[4]={0,1,0,-1};
 struct node{
     int x, y;
@@ -28,34 +28,35 @@ int bfs(int x,int y,int d){
         for(int i=0;i<4;++i){
             nx=p.x+dx[i];
             ny=p.y+dy[i];
-            if(nx>0&&nx<=n&&ny>0&&ny<=n&&b[p.x][p.y]+b[nx][ny]==1&&a[nx][ny]!=d){
+            if(nx>0&&nx<=n&&ny>0&&ny<=n&&a[p.x][p.y]+a[nx][ny]==1&&vis[nx][ny]!=d){
                 q.push({nx,ny});
-                a[nx][ny]=d;
+                vis[nx][ny]=d;
                 ans++;
             }
         }
-        a[p.x][p.y]=d;
+        vis[p.x][p.y]=d;
     }
     return ans;
 }
 int main()
 {
     cin>>n>>m;
-    int ans=0;
-    for(int i=1;i<=n;++i) for(int j=1;j<=n;++j) scanf("%1d",&a[i][j]),b[i][j]=a[i][j];
-    cnt=-1;
+    for(int i=1;i<=n;++i)
+        for(int j=1;j<=n;++j)
+            scanf("%1d",&a[i][j]);
+    cnt=1;
     for(int i=1;i<=n;++i){
         for(int j=1;j<=n;++j){
-            if(a[i][j]>=0){
-                ans=bfs(i,j,cnt);
-                an[0-cnt--]=ans;
+            if(vis[i][j]==0){
+                ans[cnt]=bfs(i,j,cnt);
+                ++cnt;
             }
         }
     }
-    int xx,yy;
+    int x,y;
     for(int i=0;i<m;++i){
-        cin>>xx>>yy;
-        cout<<an[0-a[xx][yy]]<<endl;
+        cin>>x>>y;
+        cout<<ans[vis[x][y]]<<endl;
     }
     return 0;
 }
