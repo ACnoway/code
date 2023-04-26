06082c10c31ee12fb1fc3be1686c35126d11be6c
ACnoway <imlzhyt@163.com>
2023-04-26 16:27:58 +0800

    NFLSOJ\4.26/A. mizuki 与进化

--- NFLSOJ/4.26/A. mizuki 与进化.cpp
+++ NFLSOJ/4.26/A. mizuki 与进化.cpp
@@ -25,7 +25,7 @@ inline int read(){
 const int N=200005;
 typedef pair<int,int> pii;
 int n,ha,ta,hb,tb,hc,tc;
-int a[N],b[N],c[N];
+int d[4],a[N],b[N],c[N];
 string s;
 vector<pii> ans;
 int main()
@@ -33,8 +33,15 @@ int main()
     freopen("evolution.in","r",stdin);
     freopen("evolution.out","w",stdout);
     cin>>n>>s;
-    n<<=1;
-    for(int i=0;i<n;++i){
+    for(int i=0;i<n*2;++i){
+        ++d[s[i]-'A'];
+    }
+    if(d[0]>n||d[1]>n||d[2]>n){
+        cout<<"No"<<endl;
+        return 0;
+    }
+    int ab=n-d[2],ac=n-d[1],bc=n-d[0];
+    for(int i=0;i<n*2;++i){
         if(s[i]=='A'){
             a[++ta]=i+1;
         }
@@ -42,31 +49,26 @@ int main()
             b[++tb]=i+1;
         }
         else{
-            if(ha<ta){
+            if(hb<tb&&bc){
+                --bc;
+                ans.emplace_back((pii){b[++hb],i+1});
+            }
+            else{
+                if(ha>=ta){
+                    cout<<"No"<<endl;
+                    return 0;
+                }
+                --ac;
                 ans.emplace_back((pii){a[ta--],i+1});
             }
-            else if(hb<tb){
-                ans.emplace_back((pii){b[tb--],i+1});
-            }
-            else c[++tc]=i+1;
         }
     }
-    if(hc<tc){
-        cout<<"No"<<endl;
-        return 0;
-    }
-    while(ha<ta&&hb<tb){
-        if(a[++ha]<b[++hb]){
-            ans.emplace_back((pii){a[ha],b[hb]});
-        }
-        else{
+    for(int i=1;i<=ab;++i){
+        if(a[ha+1]>b[hb+1]){
             cout<<"No"<<endl;
             return 0;
         }
-    }
-    if(ha<ta||hb<tb){
-        cout<<"No"<<endl;
-        return 0;
+        ans.emplace_back((pii){a[++ha],b[++hb]});
     }
     cout<<"Yes"<<endl;
     for(pii i:ans){
