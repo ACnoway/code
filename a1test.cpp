1b77a473e92d069700405ad8c07bf866a14fc228
ACnoway <imlzhyt@163.com>
2023-06-17 21:45:40 +0800

    template/高精度

--- a1test.cpp
+++ a1test.cpp
@@ -2,43 +2,15 @@
 #include <vector>
 using namespace std;
 
-void getSubseq(int index, vector<int>& arr, vector<vector<int>>& combineArr, vector<int> arr2) {
-	if (index >= arr.size()) {
-		combineArr.push_back(arr2);
-		return;
-	}
-	getSubseq(index + 1, arr, combineArr, arr2);
-	arr2.push_back(arr.at(index));
-	getSubseq(index + 1, arr, combineArr, arr2);
-}
-
-int getMaxModulo(vector<vector<int>>& combineArr, int m) {
-	int max = 0;
-	for (vector<int>& e : combineArr) {
-		if (e.empty()) {
-			continue;
-		}
-		long long sum = 0;
-		for (int i : e) {
-			sum += i;
-		}
-		max = max >= (sum % m) ? max : (sum % m);
-	}
-	return max;
-}
-
+struct node{
+    int a;
+    vector<int> b;
+    node():a(0){};
+    node work(const node &x){
+        return *this-x;
+    }
+}c,d;
 int main() {
-	int n = 1;
-	int m = 1;
-	cin >> n >> m;
-	vector<int> inputArr;
-	for (int i = 0; i < n; i++) {
-		int value = 0;
-		cin >> value;
-		inputArr.push_back(value);
-	}
-	vector<vector<int>> combineArr;
-	getSubseq(0, inputArr, combineArr, vector<int>());
-	cout << getMaxModulo(combineArr, m) << endl;
+    c.work();
 	return 0;
 }
